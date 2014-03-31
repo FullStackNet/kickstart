621e3ed63aba6edc8def3100599611bf0b8f8003
ajay <ajay@cloud4things.com>
2014-03-31 07:18:05 +0530

    Added creating new build file

--- src/platform/version/VersionManager.java
+++ src/platform/version/VersionManager.java
@@ -1,6 +1,5 @@
 package platform.version;
 
 public class VersionManager {
-	public static String MINOR_VERSION = "@@MINOR_VERSION@@";
-	public static String VERSION = "1.0."+MINOR_VERSION;
+	public static String VERSION = "1.0.5";
 }
