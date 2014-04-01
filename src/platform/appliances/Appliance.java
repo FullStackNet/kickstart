15c0ca129eaad01492132c1ac05769a918a677a5
ajay <ajay@cloud4things.com>
2014-04-01 10:06:10 +0530

    Appliance

---
+++ src/platform/appliances/Appliance.java
@@ -0,0 +1,14 @@
+package platform.appliances;
+
+public class  Appliance {
+	public static String APPLIANCE_VEHICLE = "VEHICLE";
+	public static String APPLIANCE_DG = "DG";
+	
+	public static String[] getList() {
+		String[] list = {
+				APPLIANCE_VEHICLE,
+				APPLIANCE_DG		
+		};
+		return list;
+	}
+}
