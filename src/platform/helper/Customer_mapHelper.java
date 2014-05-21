f1f1ad6282b1ae27203d5adb461b718060a0f164
ajay <ajay@cloud4things.com>
2014-05-21 10:06:22 +0530

    added new apis

--- src/platform/helper/Customer_mapHelper.java
+++ src/platform/helper/Customer_mapHelper.java
@@ -3,6 +3,7 @@ package platform.helper;
 import java.util.ArrayList;
 import java.util.Map;
 
+import platform.db.JoinField;
 import platform.resource.controller;
 import platform.resource.customer_map;
 import platform.resource.sensor;
@@ -33,6 +34,16 @@ public class Customer_mapHelper extends BaseHelper {
 		list = SensorHelper.getInstance().getListMapById(_customer_map.getSensors().toArray(new String[_customer_map.getSensors().size()]),
 				new String[]{sensor.FIELD_NAME}); 
 		return list;
+	}
+	
+	public ArrayList<Map<String, Object>> getControllerMapList(String customerId,ArrayList<JoinField> joinFields) {
+		ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		customer_map _customer_map = (customer_map)getSelectedFieldsById(customerId,
+				customer_map.FIELD_CONTROLLERS);
+		if ((_customer_map == null) || (_customer_map.getControllers() == null))
+			return list;
+		return ControllerHelper.getInstance().getByJoining(_customer_map.getControllers().toArray(new String[_customer_map.getControllers().size()]),
+				joinFields,new String[]{controller.FIELD_NAME});
 	}
 	
 	public ArrayList<Map<String, Object>> getControllerMapList(String customerId) {
