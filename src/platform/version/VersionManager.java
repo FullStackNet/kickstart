be70f8b189c1d7fc04a65a191ad3bc1f7a7e5d0b
WH-VM <ajay@cloud4things.com>
2014-05-31 04:23:16 -0700

    updated by build script for new build....

--- src/platform/version/VersionManager.java
+++ src/platform/version/VersionManager.java
@@ -1,6 +1,6 @@
 package platform.version;
 
 public class VersionManager {
-	public static String VERSION = "1.0.154";
+	public static String VERSION = "1.0.155";
 	public static String API_VERSION = "1.0";
 }
