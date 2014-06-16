af16b83fafa099291cd419870518d51fa9807f5c
ajay <ajay@cloud4things.com>
2014-06-17 00:19:30 +0530

    added trip detail

--- src/application/c4t/vehicle/helper/Trip_detailHelper.java
+++ src/application/c4t/vehicle/helper/Trip_detailHelper.java
@@ -1,6 +1,10 @@
 package application.c4t.vehicle.helper;
 
+import platform.db.Expression;
+import platform.db.LOG_OP;
+import platform.db.REL_OP;
 import platform.helper.BaseHelper;
+import platform.resource.BaseResource;
 import application.c4t.vehicle.resource.trip_detail;
 
 
@@ -16,5 +20,10 @@ public class Trip_detailHelper extends BaseHelper {
 		if (instance == null)
 			instance = new Trip_detailHelper();
 		return instance;
+	}
+	
+	public BaseResource[] getTripDetail(String tripId) {
+		Expression e = new Expression(trip_detail.FIELD_TRIP_ID, REL_OP.EQ, tripId);
+		return getByExpression(e,new String[]{trip_detail.FIELD_REACH_TIME});
 	}
 }
