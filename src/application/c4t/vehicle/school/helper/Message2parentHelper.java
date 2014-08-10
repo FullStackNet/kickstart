c1aa29dc8a6eeb852d7bbb18f0128936a401b469
ajay <ajay@cloud4things.com>
2014-08-10 17:47:59 +0530

    Added message2parenthelper

--- src/application/c4t/vehicle/school/helper/Message2parentHelper.java
+++ src/application/c4t/vehicle/school/helper/Message2parentHelper.java
@@ -1,6 +1,9 @@
 package application.c4t.vehicle.school.helper;
 
+import platform.db.Expression;
+import platform.db.REL_OP;
 import platform.helper.BaseHelper;
+import platform.resource.BaseResource;
 import application.c4t.vehicle.school.resource.message2parent;
 
 
@@ -18,6 +21,14 @@ public class Message2parentHelper extends BaseHelper {
 		return instance;
 	}
 	
+	public BaseResource[] getBySchool(String[] schoolIds) {
+		Expression e = new Expression(message2parent.FIELD_SCHOOL_ID,REL_OP.IN, schoolIds);
+		return getByExpression(e, new String[]{message2parent.FIELD_LAST_UPDATED +" desc"});
+	}
 	
-	
+	public BaseResource[] getByUser(String userId) {
+		String[] schoolIds = School_user_mapHelper.getInstance().getSchoolIds(userId);
+		return getBySchool(schoolIds);
+	}
+		
 }
