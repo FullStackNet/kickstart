8f689a51af6dceab1c6e195c6e423af535389dd2
ajay <ajay@cloud4things.com>
2014-08-13 14:15:07 +0530

    cortected login

--- src/platform/webservice/service/LoginService.java
+++ src/platform/webservice/service/LoginService.java
@@ -1,7 +1,9 @@
 package platform.webservice.service;
 
+import platform.helper.InviteHelper;
 import platform.helper.UserHelper;
 import platform.resource.BaseResource;
+import platform.resource.invite;
 import platform.resource.login;
 import platform.resource.user;
 import platform.util.ApplicationException;
@@ -23,14 +25,22 @@ public class LoginService extends BaseService{
 		if (_user == null)
 			_user = UserHelper.getInstance().getByMobileId(_login.getEmail_id());
 		
-		if (_user == null)
-			throw new ApplicationException(ExceptionSeverity.ERROR, "Invalid User Id or Password");
-		if (_login.getPassword().equals(_user.getPassword())) {
-			return;
+		if (_user != null) {
+			if (_login.getPassword().equals(_user.getPassword())) {
+				return;
+			}
+			String doubleMd5 = Util.doubleMD5(_login.getPassword());
+			if (doubleMd5.equals(_user.getPassword())){
+				return;
+			}
 		}
-		String doubleMd5 = Util.doubleMD5(_login.getPassword());
-		if (doubleMd5.equals(_user.getPassword())){
-			return;
+		invite _invite = InviteHelper.getInstance().validateActivtion(_login.getEmail_id(),_login.getPassword());
+		if (_invite != null) {
+			if (_user == null)
+				throw new ApplicationException(ExceptionSeverity.ERROR, _invite.getId()+"::RESET_PASSWORD");
+			else {
+				return;
+			}
 		}
 		throw new ApplicationException(ExceptionSeverity.ERROR, "Invalid User Id or Password");
 
