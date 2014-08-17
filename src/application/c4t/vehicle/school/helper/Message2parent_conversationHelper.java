3a905d7ff7594b9123aeeb7c88aa0e83df12516a
ajay <ajay@cloud4things.com>
2014-08-17 13:56:42 +0530

    message2parent conevrsation

--- src/application/c4t/vehicle/school/helper/Message2parent_conversationHelper.java
+++ src/application/c4t/vehicle/school/helper/Message2parent_conversationHelper.java
@@ -1,7 +1,11 @@
 package application.c4t.vehicle.school.helper;
 
+import platform.db.Expression;
+import platform.db.REL_OP;
 import platform.helper.BaseHelper;
+import platform.resource.BaseResource;
 import application.c4t.vehicle.school.resource.message2parent_conversation;
+import application.c4t.vehicle.school.resource.message2school_conversation;
 
 
 public class Message2parent_conversationHelper extends BaseHelper {
@@ -18,6 +22,9 @@ public class Message2parent_conversationHelper extends BaseHelper {
 		return instance;
 	}
 	
-	
+	public BaseResource[] getByThreadId(String threadId) {
+		Expression e = new Expression(message2parent_conversation.FIELD_THREAD_ID, REL_OP.EQ, threadId);
+		return getByExpression(e, new String[]{message2school_conversation.FIELD_MESSAGE_TIME});
+	}
 	
 }
