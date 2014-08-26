896e7db446a1b18ec3192158474952456a1d3121
ajay <ajay@cloud4things.com>
2014-08-26 23:36:30 +0530

    added add and modify only fields

--- src/application/c4t/vehicle/school/resource/school_class_section.java
+++ src/application/c4t/vehicle/school/resource/school_class_section.java
@@ -1,5 +1,6 @@
 package application.c4t.vehicle.school.resource;
 
+import platform.util.ApplicationConstants;
 import platform.util.Util;
 import application.c4t.vehicle.school.defined.resource.Baseschool_class_section;
 
@@ -18,6 +19,9 @@ public class school_class_section extends Baseschool_class_section {
 		this.setId(id);
 	}	
 	
+	public static String id(String school_id, String class_name,String section_name) {
+		return school_id+ApplicationConstants.ID_SEPERATOR+class_name+ApplicationConstants.ID_SEPERATOR+section_name;
+	}
 	@Override
 	public Class<?> getResultClass() {
 		// TODO Auto-generated method stub
