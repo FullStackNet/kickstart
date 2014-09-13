fcc150a491c5c6d14c8347bbfa5cc2da5a0a7980
root <root@localhost.localdomain>
2014-09-12 21:33:10 -0700

    updated by build script for new build....

--- src/platform/version/VersionManager.java
+++ src/platform/version/VersionManager.java
@@ -1,6 +1,6 @@
 package platform.version;
 
 public class VersionManager {
-	public static String VERSION = "1.0.408";
+	public static String VERSION = "1.0.409";
 	public static String API_VERSION = "2.0";
 }
