31f043bc1ef8be06fd1b4008a0af299f213445ec
Ajay Nema <nema_ajay@yahoo.com>
2014-10-08 12:35:39 +0530

    added activiation token should come correct.

--- src/application/c4t/vehicle/school/notification/ParentInviteNotificationTask.java
+++ src/application/c4t/vehicle/school/notification/ParentInviteNotificationTask.java
@@ -29,16 +29,12 @@ public class ParentInviteNotificationTask extends NotificationTask {
 
 	void sendNotification2Users(notification _notification, Map<String, BaseResource> userMap,
 			String appAlert,String smsAlert,String emailAlert,
-			String student_id,String student_name) {
-		
+			student _student,String activation_token) {
 		String school_name = "school";
-		school _school = new school();
-		student _student = (student)StudentHelper.getInstance().getById(student_id);
-		if (_student != null) {
-			_school = (school) StudentHelper.getInstance().getById(_student.getSchool_id());
-			if (_school != null)
-				school_name = _school.getName();
-		}
+		school _school;
+		_school = (school) StudentHelper.getInstance().getById(_student.getSchool_id());
+		if (_school != null)
+			school_name = _school.getName();
 		for(Map.Entry<String, BaseResource> entry : userMap.entrySet()) {
 			user _user = (user)entry.getValue();
 			if ("Y".equals(smsAlert) && (_user.getMobile_no() != null)) {
@@ -46,14 +42,15 @@ public class ParentInviteNotificationTask extends NotificationTask {
 				smsMessage.setMobile_no(_user.getMobile_no());
 				smsMessage.setType(ApplicationConstants.SMS_TYPE_INVITE_TO_JOIN_PARENT_CONNECT);
 				Map<String, String> map = new HashMap<String, String>();
-				map.put("STDEUNT_NAME", student_name);
-				map.put("STDEUNT_ID", student_id);
+				map.put("STDEUNT_NAME", _student.getName());
+				map.put("STDEUNT_ID", _student.getId());
 				map.put("SCHOOL_NAME", school_name);
 				if ((_school != null) && (_school.getBrand_name() != null)) {
 					map.put("BRAND_NAME", _school.getBrand_name());
 				} else {
 					map.put("BRAND_NAME", "School");
 				}
+				map.put("ACTIVATION_TOKEN", activation_token);
 				String params = Json.maptoString(map);
 				smsMessage.setParams(params);
 				ApplicationManager.getInstance().sendMessage(ApplicationConstants.APPLICATION_NAME_SMS_MANAGER, 
@@ -65,14 +62,15 @@ public class ParentInviteNotificationTask extends NotificationTask {
 				resendMail.setTo(_user.getEmail_id());
 				resendMail.setType(ApplicationConstants.MAIL_TYPE_INVITE_TO_JOIN_PARENT_CONNECT);
 				Map<String, String> map = new HashMap<String, String>();
-				map.put("STDEUNT_NAME", student_name);
-				map.put("STDEUNT_ID", student_id);
+				map.put("STDEUNT_ID", _student.getId());
+				map.put("SCHOOL_NAME", school_name);
 				map.put("SCHOOL_NAME", school_name);
 				if ((_school != null) && (_school.getBrand_name() != null)) {
 					map.put("BRAND_NAME", _school.getBrand_name());
 				} else {
 					map.put("BRAND_NAME", "School");
 				}
+				map.put("ACTIVATION_TOKEN", activation_token);
 				String params = Json.maptoString(map);
 				resendMail.setParams(params);
 				ApplicationManager.getInstance().sendMessage(ApplicationConstants.APPLICATION_NAME_EMAIL_MANAGER, 
@@ -85,8 +83,8 @@ public class ParentInviteNotificationTask extends NotificationTask {
 				notificationMessage.setTitle("HOMEWORK");
 				notificationMessage.setType(ApplicationConstants.NOTIFICATION_TYPE_INVITE_TO_JOIN_PARENT_CONNECT);
 				Map<String, String> map = new HashMap<String, String>();
-				map.put("STDEUNT_NAME", student_name);
-				map.put("STDEUNT_ID", student_id);
+				map.put("STDEUNT_ID", _student.getId());
+				map.put("SCHOOL_NAME", school_name);
 				String params = Json.maptoString(map);
 				notificationMessage.setParams(params);
 				ApplicationManager.getInstance().sendMessage(ApplicationConstants.APPLICATION_NAME_NOTIFICATION_MANAGER, 
@@ -101,7 +99,8 @@ public class ParentInviteNotificationTask extends NotificationTask {
 		}
 	}
 
-	void sendNotification(notification _notification,String student_id,String student_name) {
+	void sendNotification(notification _notification,String student_id,String student_name,
+			String activation_token) {
 		Map<String, BaseResource> userMap = new HashMap<String, BaseResource>();
 		String smsAlert = "N";
 		String emailAlert = "N";
@@ -132,7 +131,7 @@ public class ParentInviteNotificationTask extends NotificationTask {
 		}
 	
 		sendNotification2Users(_notification, userMap, appAlert,smsAlert,emailAlert,
-				student_id,student_name);
+				_student,activation_token);
 	}
 	@Override
 	public void process(notification _notification) {
@@ -144,7 +143,8 @@ public class ParentInviteNotificationTask extends NotificationTask {
 				return;
 			String student_id = (String)data.get("STUDENT_ID");
 			String student_name = (String)data.get("STUDENT_NAME");
-			sendNotification(_notification,student_id,student_name);
+			String activation_token = (String)data.get("ACTIVATION_TOKEN");
+			sendNotification(_notification,student_id,student_name,activation_token);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
