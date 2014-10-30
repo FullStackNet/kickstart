0bcd4c7f54000d0ae613d2e52027d5cfc2b0146e
Ajay Nema <nema_ajay@yahoo.com>
2014-10-30 13:36:18 +0530

    added api

--- src/application/c4t/vehicle/school/helper/NoticeHelper.java
+++ src/application/c4t/vehicle/school/helper/NoticeHelper.java
@@ -1,7 +1,15 @@
 package application.c4t.vehicle.school.helper;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import platform.db.Expression;
+import platform.db.JoinField;
+import platform.db.REL_OP;
 import platform.helper.BaseHelper;
+import platform.helper.HelperFactory;
 import platform.util.ApplicationException;
+import application.c4t.vehicle.school.resource.daily_activity;
 import application.c4t.vehicle.school.resource.notice;
 
 
@@ -22,5 +30,15 @@ public class NoticeHelper extends BaseHelper {
 		notice _notice = new notice(id);
 		_notice.setSent("Y");
 		update(_notice);
+	}
+	
+	public ArrayList<Map<String, Object>> getForSchools(String[] schools) throws ApplicationException  {
+		HelperFactory.getInstance().register(SchoolHelper.getInstance());
+		HelperFactory.getInstance().register(NoticeHelper.getInstance());
+		ArrayList<JoinField> list = new ArrayList<JoinField>();
+		JoinField field = new JoinField("school", "school_id", "school_name");
+		list.add(field);
+		Expression e = new Expression(daily_activity.FIELD_SCHOOL_ID, REL_OP.IN, schools);
+		return getByJoining(e,list, new String[]{notice.FIELD_NOTICE_DATE + " desc"});
 	}
 }
