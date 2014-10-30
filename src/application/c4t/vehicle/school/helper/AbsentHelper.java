bf66f6d422ab6943da79d707ef4f8e1757770297
Ajay Nema <nema_ajay@yahoo.com>
2014-10-30 14:33:40 +0530

    added

--- src/application/c4t/vehicle/school/helper/AbsentHelper.java
+++ src/application/c4t/vehicle/school/helper/AbsentHelper.java
@@ -1,9 +1,14 @@
 package application.c4t.vehicle.school.helper;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import platform.db.Expression;
+import platform.db.JoinField;
 import platform.db.LOG_OP;
 import platform.db.REL_OP;
 import platform.helper.BaseHelper;
+import platform.helper.HelperFactory;
 import platform.resource.BaseResource;
 import platform.util.ApplicationException;
 import application.c4t.vehicle.school.resource.absent;
@@ -38,8 +43,19 @@ public class AbsentHelper extends BaseHelper {
 		return getByExpression(e5, new String[]{absent.FIELD_DATE+ " desc"});
 	}
 	
-	public BaseResource[] getForSchools(String[] schools) {
+	public ArrayList<Map<String, Object>> getForSchools(String[] schools) {
+		HelperFactory.getInstance().register(SchoolHelper.getInstance());
+		HelperFactory.getInstance().register(AbsentHelper.getInstance());
+		ArrayList<JoinField> list = new ArrayList<JoinField>();
+		JoinField field = new JoinField("school", "school_id", "school_name");
+		list.add(field);
 		Expression e = new Expression(absent.FIELD_SCHOOL_ID, REL_OP.IN, schools);
-		return getByExpression(e, new String[]{absent.FIELD_DATE + " desc"});
+		try {
+			return getByJoining(e,list, new String[]{absent.FIELD_DATE+ " desc"});
+		} catch (ApplicationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return new ArrayList<Map<String, Object>>();
 	}
 }
