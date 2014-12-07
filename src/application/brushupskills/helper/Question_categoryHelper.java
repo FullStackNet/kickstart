ded2418b13ab65be6b368cabd275124cc66ea96f
Ajay Nema <nema_ajay@yahoo.com>
2014-12-07 22:58:52 +0530

    order

--- src/application/brushupskills/helper/Question_categoryHelper.java
+++ src/application/brushupskills/helper/Question_categoryHelper.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.mongodb.util.Hash;
-
 import platform.helper.BaseHelper;
 import platform.resource.BaseResource;
 import platform.util.ApplicationException;
 import platform.util.Util;
 import platform.webservice.ui.definition.IdValue;
-import application.brushupskills.resource.question;
 import application.brushupskills.resource.question_category;
 
 public class Question_categoryHelper extends BaseHelper {
@@ -83,4 +80,42 @@ public class Question_categoryHelper extends BaseHelper {
 		addChilds(parentMap,list,child_list,level);
 		return list.toArray(new IdValue[list.size()]);
 	}
+	
+	void addLevelResource(Map<String, ArrayList<question_category>> parentMap,ArrayList<BaseResource> list,ArrayList<question_category> child_list, int level) {
+		if (Util.isEmpty(child_list))
+			return;
+		for(int i=0; i < child_list.size(); i++) {
+			String name = getLevelName(child_list.get(i).getName(), level);
+			child_list.get(i).setName(name);
+			list.add(child_list.get(i));
+			ArrayList<question_category> child_child_list = parentMap.get(child_list.get(i).getId());
+			if (!Util.isEmpty(child_child_list)) {
+				addLevelResource(parentMap,list,child_child_list,level+1);
+			}
+		}
+	}
+	
+	public BaseResource[] getLeveledResources() {
+		int level = 0;
+		ArrayList<BaseResource> list = new ArrayList<>();
+		BaseResource[] resources = Question_categoryHelper.getInstance().getAll(new String[]{question_category.FIELD_ORDER});
+		Map<String, ArrayList<question_category>> parentMap = new HashMap<String, ArrayList<question_category>>();
+		for(BaseResource resource : resources) {
+			String parent = "root";
+			question_category _catgory = (question_category)resource;
+			if (!Util.isEmpty(_catgory.getParent_category_id())) {
+				parent = _catgory.getParent_category_id();
+			}
+			ArrayList<question_category> child_list = parentMap.get(parent);
+			if (Util.isEmpty(child_list)) {
+				child_list = new ArrayList<question_category>();
+			}
+			child_list.add(_catgory);
+			parentMap.put(parent,child_list);
+		}
+		ArrayList<question_category> child_list = parentMap.get("root");
+		addLevelResource(parentMap,list,child_list,level);
+		return list.toArray(new question_category[list.size()]);
+	}
+	
 }
