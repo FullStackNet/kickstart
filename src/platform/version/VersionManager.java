b5d00d449cfa1d23658cfe6e7188b494693419e2
build <ajay@cloud4things.com>
2014-12-16 11:04:09 +0530

    updated by build script for new build....

--- src/platform/version/VersionManager.java
+++ src/platform/version/VersionManager.java
@@ -1,6 +1,6 @@
 package platform.version;
 
 public class VersionManager {
-	public static String VERSION = "1.0.531";
+	public static String VERSION = "1.0.532";
 	public static String API_VERSION = "2.0";
 }
