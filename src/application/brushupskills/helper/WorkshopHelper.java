647d00b0bdc524d34e1e8e84a6e6ae7d6ea1d043
Ajay Nema <nema_ajay@yahoo.com>
2014-12-19 22:14:33 +0530

    added

--- src/application/brushupskills/helper/WorkshopHelper.java
+++ src/application/brushupskills/helper/WorkshopHelper.java
@@ -1,6 +1,9 @@
 package application.brushupskills.helper;
 
+import platform.db.Expression;
+import platform.db.REL_OP;
 import platform.helper.BaseHelper;
+import platform.resource.BaseResource;
 import application.brushupskills.resource.workshop;
 
 public class WorkshopHelper extends BaseHelper {
@@ -16,5 +19,10 @@ public class WorkshopHelper extends BaseHelper {
 	public WorkshopHelper() {
 		super(new workshop());
 		// TODO Auto-generated constructor stub
+	}
+	
+	public BaseResource[] getNextWorkshop(long time) {
+		Expression e = new Expression(workshop.FIELD_WORKSHOP_DATE, REL_OP.GT, time);
+		return getByExpression(e,new String[]{});	
 	}
 }
