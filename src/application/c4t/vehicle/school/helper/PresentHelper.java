16a782946a71f3c6ffca1ea49f15fa0507b107ff
Ajay Nema <nema_ajay@yahoo.com>
2015-01-14 19:37:04 +0530

    added present

--- src/application/c4t/vehicle/school/helper/PresentHelper.java
+++ src/application/c4t/vehicle/school/helper/PresentHelper.java
@@ -212,9 +212,10 @@ public class PresentHelper extends BaseHelper {
 			_detail.setDate(TimeUtil.getTimeFromDateString(null, today));
 			Present_detailHelper.getInstance().add(_detail);
 			_detail = (present_detail)Present_detailHelper.getInstance().getById(entryKeyDetail); 
+			updateTotalPresent(entryKey);
 			return;
 		} 
-		updateTotalPresent(entryKey);
+		
 		if ((currentTime - _detail.getCreation_time()) < 60*1000L) {
 			return;
 		}
