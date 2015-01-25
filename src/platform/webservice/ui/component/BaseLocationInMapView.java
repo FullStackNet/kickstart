4fe62f9a5290d0087191dbddd77f4547a37e9209
Ajay Nema <nema_ajay@yahoo.com>
2015-01-25 18:18:00 +0530

    600px

--- src/platform/webservice/ui/component/BaseLocationInMapView.java
+++ src/platform/webservice/ui/component/BaseLocationInMapView.java
@@ -30,7 +30,7 @@ public abstract class BaseLocationInMapView extends BaseView {
 		populateData();
 		Div div = new Div();
 		div.addAttribute("id",mDefinition.getId());
-		div.addAttribute("style","font-size : 10px;width: 100%; height: 100%");
+		div.addAttribute("style","font-size : 10px;width: 100%; height: 600px");
 		getView().addChild(div);
 		StringBuffer buffer = new StringBuffer();
 		buffer.append("<script type=\"text/javascript\">\n");
