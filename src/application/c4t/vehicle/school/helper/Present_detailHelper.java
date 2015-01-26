0931c9f9e4552f80a2003750e612e9d52641bea4
Ajay Nema <nema_ajay@yahoo.com>
2015-01-26 16:24:35 +0530

    added queries

--- src/application/c4t/vehicle/school/helper/Present_detailHelper.java
+++ src/application/c4t/vehicle/school/helper/Present_detailHelper.java
@@ -5,12 +5,13 @@ import java.util.Map;
 
 import platform.db.Expression;
 import platform.db.JoinField;
+import platform.db.LOG_OP;
 import platform.db.REL_OP;
 import platform.helper.BaseHelper;
 import platform.resource.BaseResource;
 import platform.util.ApplicationException;
 import platform.util.Util;
-import application.c4t.vehicle.school.resource.absent_detail;
+import application.c4t.vehicle.school.resource.present_detail;
 import application.c4t.vehicle.school.resource.present_detail;
 
 
@@ -40,7 +41,7 @@ public class Present_detailHelper extends BaseHelper {
 		}
 		String[] ids = new String[resources.length];
 		for(int i=0; i < resources.length; i++) {
-			absent_detail detail = (absent_detail)resources[i];
+			present_detail detail = (present_detail)resources[i];
 			ids[i] = detail.getStudent_id();
 		}
 		return StudentHelper.getInstance().getById(ids, null);
@@ -48,10 +49,23 @@ public class Present_detailHelper extends BaseHelper {
 	
 
 	public BaseResource[] getByStudent(String studentId) {
-		Expression e = new Expression(absent_detail.FIELD_STUDENT_ID, REL_OP.EQ, studentId);
+		Expression e = new Expression(present_detail.FIELD_STUDENT_ID, REL_OP.EQ, studentId);
 		return getByExpression(e,new String[]{present_detail.FIELD_DATE +" desc"});
 	}
+	public BaseResource[] getByBusStudent(String studentId) {
+		Expression e1 = new Expression(present_detail.FIELD_STUDENT_ID, REL_OP.EQ, studentId);
+		Expression e2 = new Expression(present_detail.FIELD_PRESENT_TYPE, REL_OP.EQ, "BUS");
+		Expression e = new Expression(e1, LOG_OP.AND, e2);
+		return getByExpression(e,new String[]{present_detail.FIELD_DATE +" desc" , present_detail.FIELD_CREATION_TIME + " desc"});
+	}
 
+	public BaseResource[] getBySchoolStudent(String studentId) {
+		Expression e1 = new Expression(present_detail.FIELD_STUDENT_ID, REL_OP.EQ, studentId);
+		Expression e2 = new Expression(present_detail.FIELD_PRESENT_TYPE, REL_OP.EQ, "SCHOOL");
+		Expression e = new Expression(e1, LOG_OP.AND, e2);
+		return getByExpression(e,new String[]{present_detail.FIELD_DATE +" desc", present_detail.FIELD_CREATION_TIME + " desc"});
+	}
+	
 	public ArrayList<Map<String, Object>> getDetailListMap(String absent_parent_id,ArrayList<JoinField> joinFields) {
 		ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		Expression expression = new Expression(present_detail.FIELD_PRESENT_PARENT_ID, REL_OP.EQ,absent_parent_id);
