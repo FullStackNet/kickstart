e70036d4f4691fc8f8258107cd9bd1a76801c3f2
Ajay Nema <nema_ajay@yahoo.com>
2015-02-24 00:10:49 +0530

    added open/close menu

--- src/platform/webservice/ui/layout/TwoColumnLayout.java
+++ src/platform/webservice/ui/layout/TwoColumnLayout.java
@@ -1,9 +1,11 @@
 package platform.webservice.ui.layout;
 
 import platform.webservice.ui.UIServletContext;
+import platform.webservice.ui.html.A;
 import platform.webservice.ui.html.BaseHTMLComponent;
 import platform.webservice.ui.html.CSS;
 import platform.webservice.ui.html.Div;
+import platform.webservice.ui.html.IMG;
 import platform.webservice.ui.html.UL;
 import platform.webservice.ui.util.Attribute;
 
@@ -35,10 +37,22 @@ public class TwoColumnLayout extends BaseLayout {
 		mLayoutLeft = new Div();
 		mLayoutLeft.addAttribute(new Attribute("id", "left-column"));
 		content_container_2.addChild(mLayoutLeft);
-		
-		
 		mLayoutContent = new Div();
 		mLayoutContent.addAttribute(new Attribute("id", "content"));
+		Div div  = new Div();
+		div.addAttribute("style", "float:left;height:100%;");
+		A a = new A();
+		a.addAttribute("style","top:400px");
+		IMG img = new IMG();
+		img.addAttribute("id", "open-menu-image-id");
+		img.setSRC("/ui/images/close-menu.png");
+		img.addAttribute("width","20px");
+		img.addAttribute("height","20px");
+		a.addChild(img);
+		a.setHref("#");
+		a.addAttribute("onClick","closeMenu()");
+		div.addChild(a);
+		content_container_2.addChild(div);
 		content_container_2.addChild(mLayoutContent);
 		
 		//mLayoutFooter = new Div();
