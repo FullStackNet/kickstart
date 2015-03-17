cc65212ced560831487d292914bd9724318d0236
Ajay Nema <nema_ajay@yahoo.com>
2015-03-17 11:40:22 +0530

    added parmanent message producer and queue

--- src/platform/communication/messagebus/Connector.java
+++ src/platform/communication/messagebus/Connector.java
@@ -36,7 +36,7 @@ public class Connector {
 	String name;
 	Connection connection;
 	Map<String, MessageProducer> peerMap;
-	
+
 	ATMQDecoder decoder;
 	ATMQEncoder encoder;
 	Thread  messageProcessingThread;
@@ -45,7 +45,7 @@ public class Connector {
 	ApplicationThreadPool pool;
 	int numberOfmessageProcessingThread;
 	int capacityOfmessageProcessingQueue;
-	
+
 	public Connector(String connectURL, String name, String userName, String password, MessageManager messageManager) {
 		this.connectURL = connectURL;
 		this.userName = userName;
@@ -58,18 +58,18 @@ public class Connector {
 		this.messageManager = messageManager;
 		numberOfmessageProcessingThread = 10;
 		capacityOfmessageProcessingQueue = 15;
-		
+
 	}
-	
+
 	class MessageProcessingTask implements Runnable {
 		platform.communication.Session session;
 		platform.message.Message message;
-		
+
 		public MessageProcessingTask(platform.communication.Session session , platform.message.Message message) {
 			this.session = session;
 			this.message = message;
 		}
-		
+
 		void process_message() {
 			ApplicationLogger.info("Recieved the Message " + message.getName()+"("+message.getPacketType()+")"  + " from " + message.getSender(), this.getClass());
 			if (message.getPacketType() == platform.message.Message.PACKET_TYPE_REQUEST) {
@@ -81,27 +81,27 @@ public class Connector {
 					sendMessage(response);
 				}
 			} else if (message.getPacketType() == platform.message.Message.PACKET_TYPE_RESPONSE) {
-				 message.request_process(session, null);
+				message.request_process(session, null);
 			}
 		}
-		
+
 		public void run() {
 			process_message();
 		}
 	}
-	
-	
+
+
 	public void start() {
 		pool = new ApplicationThreadPool("CONNECTOR",numberOfmessageProcessingThread, capacityOfmessageProcessingQueue);
 		pool.start();
 
 		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(connectURL);
-	  	((ActiveMQConnectionFactory)connectionFactory).setUseAsyncSend(true);
-    	 // Create a Connection
-      try {
-    	  	connection = connectionFactory.createConnection();
-    	  	connection.setExceptionListener(new ExceptionListener() {
-				
+		((ActiveMQConnectionFactory)connectionFactory).setUseAsyncSend(true);
+		// Create a Connection
+		try {
+			connection = connectionFactory.createConnection();
+			connection.setExceptionListener(new ExceptionListener() {
+
 				@Override
 				public void onException(JMSException exception) {
 					// TODO Auto-generated method stub
@@ -112,8 +112,26 @@ public class Connector {
 			connection.start();
 			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			Queue queue = session.createQueue("MessageQueue."+name);
+			Queue permanentQueue = session.createQueue("MessageQueue.permanent."+name);
 			MessageConsumer consumer = session.createConsumer(queue);
+			MessageConsumer permanentConsumer = session.createConsumer(permanentQueue);
 			ApplicationLogger.info("Creating Queue " + "MessageQueue."+name, this.getClass());
+			permanentConsumer.setMessageListener(new MessageListener() {
+				@Override
+				public void onMessage(javax.jms.Message message) {
+					// TODO Auto-generated method stub
+					platform.communication.Session cr4Session = null ;
+					platform.message.Message msg = decoder.decode(message,messageManager);
+					if (msg == null) {
+						// send error to sender
+						return;
+					}
+					ApplicationLogger.info(msg.getSender()+" Recieved Permanent Message "+msg.getName()+" from " + msg.getSender()+ "\n\t"+msg.getDump(), this.getClass());
+					cr4Session = SessionManager.getInstance().getSession(msg.getSender());
+					//process_message(cr4Session, msg);
+					pool.addTask(new MessageProcessingTask(cr4Session, msg));
+				}
+			});
 			consumer.setMessageListener(new MessageListener() {
 				@Override
 				public void onMessage(javax.jms.Message message) {
@@ -145,7 +163,7 @@ public class Connector {
 				}
 			}; 
 			EventManager.getInstance().registerEventListener(processShutDownListner, ShutDownProcessEvent.class);
-		
+
 			if (messageProcessingThread == null) {
 				messageProcessingThread = new Thread(new Runnable() {
 					@Override
@@ -172,8 +190,8 @@ public class Connector {
 			e.printStackTrace();
 			GlobalDataManager.getInstance().setConnected(false);
 		}
- 	}
-	
+	}
+
 	public Message getMessage() {
 		Message message = null;
 		try {
@@ -184,27 +202,44 @@ public class Connector {
 		}
 		return message;
 	}
-	
+
 	public void sendMessage(platform.message.Message message) {
 		if (message == null)
 			return;
 		String targetId = message.getTarget();
-		MessageProducer producer = peerMap.get(targetId);
-		if (producer == null) {
-			addPeer(targetId);
-			producer = peerMap.get(targetId);
-		}
-		try {
-			 ApplicationLogger.info(message.getSender()+" Sending Message "+message.getName()+" to " + targetId + "\n"+message.getDump(), this.getClass());
-			 Message msg = getMessage();
-			 encoder.encode(msg, message);
-			 producer.send(msg);
-		} catch (JMSException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (message.isPersistent()) {
+			MessageProducer producer = peerMap.get("permament_"+targetId);
+			if (producer == null) {
+				addPeer(targetId);
+				producer = peerMap.get("permament_"+targetId);
+			}
+			try {
+				ApplicationLogger.info(message.getSender()+" Sending Permament Message "+message.getName()+" to " + targetId + "\n"+message.getDump(), this.getClass());
+				Message msg = getMessage();
+				encoder.encode(msg, message);
+				producer.send(msg);
+			} catch (JMSException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			MessageProducer producer = peerMap.get(targetId);
+			if (producer == null) {
+				addPeer(targetId);
+				producer = peerMap.get(targetId);
+			}
+			try {
+				ApplicationLogger.info(message.getSender()+" Sending Message "+message.getName()+" to " + targetId + "\n"+message.getDump(), this.getClass());
+				Message msg = getMessage();
+				encoder.encode(msg, message);
+				producer.send(msg);
+			} catch (JMSException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
-	
+
 	public void addPeer(String peerId) {
 		MessageProducer peer = peerMap.get(peerId);
 		if (peer != null)
@@ -212,28 +247,36 @@ public class Connector {
 		try {
 			Destination destination = session.createQueue("MessageQueue."+peerId);
 			peer = session.createProducer(destination);
-	        peer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-	        peerMap.put(peerId, peer);
-	 	} catch(Exception e) {
-			
+			peer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+			peerMap.put(peerId, peer);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			Destination parmanentDestination = session.createQueue("MessageQueue.permanent."+peerId);
+			MessageProducer paramanentPeer = session.createProducer(parmanentDestination);
+			paramanentPeer.setDeliveryMode(DeliveryMode.PERSISTENT);
+			peerMap.put("permament_"+peerId, paramanentPeer);
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	public void disconnect() {
-		 try {
+		try {
 			session.close();
 		} catch (JMSException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-         try {
+		try {
 			connection.close();
 		} catch (JMSException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public String getName() {
 		return name;
 	}
