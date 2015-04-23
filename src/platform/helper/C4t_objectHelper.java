b5c6b5c6d97b034160d74c79400a4d54a3a251b6
Ajay Nema <nema_ajay@yahoo.com>
2015-04-23 16:52:46 +0530

    added

---
+++ src/platform/helper/C4t_objectHelper.java
@@ -0,0 +1,19 @@
+package platform.helper;
+
+import platform.resource.c4t_object;
+
+
+public class C4t_objectHelper extends BaseHelper {
+	
+	public C4t_objectHelper() {
+		super(new c4t_object());
+		// TODO Auto-generated constructor stub
+	}
+	private static C4t_objectHelper instance;
+	
+	public static C4t_objectHelper getInstance() {
+		if (instance == null)
+			instance = new C4t_objectHelper();
+		return instance;
+	}
+}
