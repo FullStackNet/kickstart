4a690af36019304e54c20db5a8755f436ead091d
Ajay Nema <nema_ajay@yahoo.com>
2015-04-23 16:42:13 +0530

    added c4t Object

---
+++ src/platform/defined/resource/Basec4t_relation.java
@@ -0,0 +1,447 @@
+/*
+ * Copyright 2010-2020 M16, Inc. All rights reserved.
+ * This software and documentation contain valuable trade
+ * secrets and proprietary property belonging to M16, Inc.
+ * None of this software and documentation may be copied,
+ * duplicated or disclosed without the express
+ * written permission of M16, Inc.
+ */
+
+package platform.defined.resource;
+
+import platform.resource.BaseResource;
+import platform.util.*;
+import platform.db.*;
+import java.util.*;
+
+/*
+ ********** This is a generated class **********
+ * 
+ */
+public abstract class Basec4t_relation extends BaseResource {
+	private String id = null;
+	private String to_id = null;
+	private String type = null;
+	private String user_id = null;
+	private String name = null;
+	private String email_id = null;
+	private String mobile_no = null;
+	private String status = null;
+	private Long creation_time = null;
+	private String role = null;
+	private Map<String, Object> extra_data = null;
+
+	public static String FIELD_ID = "id";
+	public static String FIELD_TO_ID = "to_id";
+	public static String FIELD_TYPE = "type";
+	public static String FIELD_USER_ID = "user_id";
+	public static String FIELD_NAME = "name";
+	public static String FIELD_EMAIL_ID = "email_id";
+	public static String FIELD_MOBILE_NO = "mobile_no";
+	public static String FIELD_STATUS = "status";
+	public static String FIELD_CREATION_TIME = "creation_time";
+	public static String FIELD_ROLE = "role";
+	public static String FIELD_EXTRA_DATA = "extra_data";
+
+	private static final long serialVersionUID = 1L;
+	private final static ResourceMetaData metaData = new ResourceMetaData("c4t_relation");
+
+	static {
+		metaData.setCheckBeforeAdd(false);
+		metaData.setCheckBeforeUpdate(false);
+		Field idField = new Field("id", "String");
+		idField.setRequired(true);
+		idField.setLength(128);
+		metaData.addField(idField);
+
+		Field to_idField = new Field("to_id", "String");
+		to_idField.setRequired(true);
+		to_idField.setLength(128);
+		metaData.addField(to_idField);
+
+		Field typeField = new Field("type", "String");
+		typeField.setRequired(true);
+		typeField.setLength(128);
+		metaData.addField(typeField);
+
+		Field user_idField = new Field("user_id", "String");
+		user_idField.setLength(128);
+		metaData.addField(user_idField);
+
+		Field nameField = new Field("name", "String");
+		nameField.setLength(128);
+		metaData.addField(nameField);
+
+		Field email_idField = new Field("email_id", "String");
+		email_idField.setLength(128);
+		metaData.addField(email_idField);
+
+		Field mobile_noField = new Field("mobile_no", "String");
+		mobile_noField.setLength(128);
+		metaData.addField(mobile_noField);
+
+		Field statusField = new Field("status", "String");
+		statusField.setLength(128);
+		metaData.addField(statusField);
+
+		Field creation_timeField = new Field("creation_time", "timestamp");
+		metaData.addField(creation_timeField);
+
+		Field roleField = new Field("role", "String");
+		roleField.setLength(128);
+		metaData.addField(roleField);
+
+		Field extra_dataField = new Field("extra_data", "Map");
+		extra_dataField.setValueType("Object");
+		metaData.addField(extra_dataField);
+
+
+		metaData.setTableName("c4t_relation");
+
+		metaData.setCluster("DB_OBJECT");
+	}
+
+	public Basec4t_relation() {}
+
+	public Basec4t_relation(Basec4t_relation obj) {
+		this.id = obj.id;
+		this.to_id = obj.to_id;
+		this.type = obj.type;
+		this.user_id = obj.user_id;
+		this.name = obj.name;
+		this.email_id = obj.email_id;
+		this.mobile_no = obj.mobile_no;
+		this.status = obj.status;
+		this.creation_time = obj.creation_time;
+		this.role = obj.role;
+		this.extra_data = obj.extra_data;
+	}
+
+	public ResourceMetaData getMetaData() {
+		return metaData;
+	}
+
+	public Map<String, Object> convertResourceToMap() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if(id != null)
+			map.put("id", id);
+		if(to_id != null)
+			map.put("to_id", to_id);
+		if(type != null)
+			map.put("type", type);
+		if(user_id != null)
+			map.put("user_id", user_id);
+		if(name != null)
+			map.put("name", name);
+		if(email_id != null)
+			map.put("email_id", email_id);
+		if(mobile_no != null)
+			map.put("mobile_no", mobile_no);
+		if(status != null)
+			map.put("status", status);
+		if(creation_time != null)
+			map.put("creation_time", creation_time);
+		if(role != null)
+			map.put("role", role);
+		if(extra_data != null)
+			map.put("extra_data", extra_data);
+		return map;
+	}
+
+	public Map<String, Object> validateAndConvertResourceToMap(boolean add) throws ApplicationException {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if(validateId(add))
+			map.put("id", id);
+		if(validateTo_id(add))
+			map.put("to_id", to_id);
+		if(validateType(add))
+			map.put("type", type);
+		if(user_id != null)
+			map.put("user_id", user_id);
+		if(name != null)
+			map.put("name", name);
+		if(email_id != null)
+			map.put("email_id", email_id);
+		if(mobile_no != null)
+			map.put("mobile_no", mobile_no);
+		if(status != null)
+			map.put("status", status);
+		if(creation_time != null)
+			map.put("creation_time", creation_time);
+		if(role != null)
+			map.put("role", role);
+		if(extra_data != null)
+			map.put("extra_data", extra_data);
+		return map;
+	}
+
+	public Map<String, Object> convertResourceToPrimaryMap() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		return map;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void convertMapToResource(Map<String, Object> map) {
+		id = (String) map.get("id");
+		to_id = (String) map.get("to_id");
+		type = (String) map.get("type");
+		user_id = (String) map.get("user_id");
+		name = (String) map.get("name");
+		email_id = (String) map.get("email_id");
+		mobile_no = (String) map.get("mobile_no");
+		status = (String) map.get("status");
+		creation_time = (Long) map.get("creation_time");
+		role = (String) map.get("role");
+		extra_data = (Map<String, Object>) map.get("extra_data");
+	}
+
+	@SuppressWarnings("unchecked")
+	public void convertTypeUnsafeMapToResource(Map<String, Object> map) {
+		Object idObj = map.get("id");
+		if(idObj != null)
+			id = idObj.toString();
+
+		Object to_idObj = map.get("to_id");
+		if(to_idObj != null)
+			to_id = to_idObj.toString();
+
+		Object typeObj = map.get("type");
+		if(typeObj != null)
+			type = typeObj.toString();
+
+		Object user_idObj = map.get("user_id");
+		if(user_idObj != null)
+			user_id = user_idObj.toString();
+
+		Object nameObj = map.get("name");
+		if(nameObj != null)
+			name = nameObj.toString();
+
+		Object email_idObj = map.get("email_id");
+		if(email_idObj != null)
+			email_id = email_idObj.toString();
+
+		Object mobile_noObj = map.get("mobile_no");
+		if(mobile_noObj != null)
+			mobile_no = mobile_noObj.toString();
+
+		Object statusObj = map.get("status");
+		if(statusObj != null)
+			status = statusObj.toString();
+
+		Object creation_timeObj = map.get("creation_time");
+		if(creation_timeObj != null)
+			creation_time = (Long) creation_timeObj;
+
+		Object roleObj = map.get("role");
+		if(roleObj != null)
+			role = roleObj.toString();
+
+		extra_data = (Map<String, Object>) map.get("extra_data");
+	}
+
+	public void convertPrimaryMapToResource(Map<String, Object> map) {
+	}
+
+	public void convertTypeUnsafePrimaryMapToResource(Map<String, Object> map) {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getIdEx() {
+		return id != null ? id : "";
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void unSetId() {
+		this.id = null;
+	}
+
+	public boolean validateId(boolean add) throws ApplicationException {
+		if(add && id == null)
+			throw new ApplicationException(ExceptionSeverity.ERROR, "Requierd validation Failed[id]");
+		return id != null;
+	}
+
+	public String getTo_id() {
+		return to_id;
+	}
+
+	public String getTo_idEx() {
+		return to_id != null ? to_id : "";
+	}
+
+	public void setTo_id(String to_id) {
+		this.to_id = to_id;
+	}
+
+	public void unSetTo_id() {
+		this.to_id = null;
+	}
+
+	public boolean validateTo_id(boolean add) throws ApplicationException {
+		if(add && to_id == null)
+			throw new ApplicationException(ExceptionSeverity.ERROR, "Requierd validation Failed[to_id]");
+		return to_id != null;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getTypeEx() {
+		return type != null ? type : "";
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void unSetType() {
+		this.type = null;
+	}
+
+	public boolean validateType(boolean add) throws ApplicationException {
+		if(add && type == null)
+			throw new ApplicationException(ExceptionSeverity.ERROR, "Requierd validation Failed[type]");
+		return type != null;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public String getUser_idEx() {
+		return user_id != null ? user_id : "";
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public void unSetUser_id() {
+		this.user_id = null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getNameEx() {
+		return name != null ? name : "";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void unSetName() {
+		this.name = null;
+	}
+
+	public String getEmail_id() {
+		return email_id;
+	}
+
+	public String getEmail_idEx() {
+		return email_id != null ? email_id : "";
+	}
+
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+
+	public void unSetEmail_id() {
+		this.email_id = null;
+	}
+
+	public String getMobile_no() {
+		return mobile_no;
+	}
+
+	public String getMobile_noEx() {
+		return mobile_no != null ? mobile_no : "";
+	}
+
+	public void setMobile_no(String mobile_no) {
+		this.mobile_no = mobile_no;
+	}
+
+	public void unSetMobile_no() {
+		this.mobile_no = null;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public String getStatusEx() {
+		return status != null ? status : "";
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void unSetStatus() {
+		this.status = null;
+	}
+
+	public Long getCreation_time() {
+		return creation_time;
+	}
+
+	public void setCreation_time(Long creation_time) {
+		this.creation_time = creation_time;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+	public String getRoleEx() {
+		return role != null ? role : "";
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public void unSetRole() {
+		this.role = null;
+	}
+
+	public Map<String, Object> getExtra_data() {
+		return extra_data;
+	}
+
+	public Object getExtra_data(String key) {
+		return extra_data == null ? null : extra_data.get(key);
+	}
+
+	public void setExtra_data(Map<String, Object> extra_data) {
+		this.extra_data = extra_data;
+	}
+
+	public void setExtra_data(String key, Object value) {
+		if(extra_data == null)
+			extra_data = new HashMap<String, Object>();
+		extra_data.put(key, value);
+	}
+
+	public void unSetExtra_data() {
+		this.extra_data = null;
+	}
+	public String getCluster() {
+		return "DB_OBJECT";
+	}
+	public String getClusterType() {
+		return "REPLICATED";
+	}
+}
