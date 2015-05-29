5afa32e52de35863cb84cb6e51ca6f4f61216094
Ajay Nema <nema_ajay@yahoo.com>
2015-05-29 13:59:36 +0530

    account resources

--- src/application/c4t/vehicle/school/defined/ResourcesToGenerate.java
+++ src/application/c4t/vehicle/school/defined/ResourcesToGenerate.java
@@ -103,7 +103,10 @@ public class ResourcesToGenerate {
 		new ResourceMap("fee_discount", "application.c4t.vehicle.school.resource.fee_discount"),	
 		new ResourceMap("holiday", "application.c4t.vehicle.school.resource.holiday"),	
 		new ResourceMap("account_heading", "application.c4t.vehicle.school.resource.account_heading"),	
-		
+		new ResourceMap("account", "application.c4t.vehicle.school.resource.account"),	
+		new ResourceMap("account_year", "application.c4t.vehicle.school.resource.account_year"),	
+		new ResourceMap("account_transaction", "application.c4t.vehicle.school.resource.account_transaction"),	
+				
 
 	};
 }
