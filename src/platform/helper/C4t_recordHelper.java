bc27671431f0f5b265c71584310eeb7c74ca364a
Ajay Nema <nema_ajay@yahoo.com>
2015-06-13 10:31:25 +0530

    added expression

--- src/platform/helper/C4t_recordHelper.java
+++ src/platform/helper/C4t_recordHelper.java
@@ -96,6 +96,38 @@ public class C4t_recordHelper extends BaseHelper {
 		
 	}
 	
+	
+	public ArrayList<Map<String, Object>> getByListMapUserIdWithCommunityName(String userId,String record_type,
+			String[] order,long from_time, long to_time, Expression expression) {
+			String[] ids = User_mapHelper.getInstance().getCommunityIds(userId);
+			ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+			HelperFactory.getInstance().register(this);
+			Expression e1 = new Expression(c4t_record.FIELD_RECORD_TYPE, REL_OP.EQ, record_type);
+			Expression e2 = new Expression(c4t_record.FIELD_COMMUNITY_ID, REL_OP.IN, ids);
+			Expression e3 = new Expression(e1, LOG_OP.AND, e2);
+			Expression e4 = new Expression(c4t_record.FIELD_EVENT_DATE, REL_OP.GTEQ, from_time);
+			Expression e5 = new Expression(c4t_record.FIELD_EVENT_DATE, REL_OP.LT, to_time);
+			Expression e6 = new Expression(e4, LOG_OP.AND, e5);
+			Expression e7 = new Expression(e3, LOG_OP.AND, e6);
+			Expression e = e7;
+			if (expression != null) {
+				e = new Expression(e7, LOG_OP.AND, expression);
+			}
+				
+			ArrayList<JoinField> joinList = new ArrayList<>();
+			JoinField jfield = new JoinField("c4t_object", c4t_record.FIELD_COMMUNITY_ID, "community_name");
+			joinList.add(jfield);
+			try {
+				list =  getByJoining(e,joinList,order);
+				return list;
+			} catch (ApplicationException exp1) {
+				// TODO Auto-generated catch block
+				exp1.printStackTrace();
+			}
+			return list;
+			
+	}
+	
 	public c4t_record getById(String id) {
 		return (c4t_record)super.getById(id);
 	}
