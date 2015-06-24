2eb8d596b62c5e30ab46130a3a3c3c1f0b3fd4e7
Ajay Nema <nema_ajay@yahoo.com>
2015-06-24 23:40:35 +0530

    logo

--- src/platform/webservice/ui/util/ImageUtils.java
+++ src/platform/webservice/ui/util/ImageUtils.java
@@ -6,6 +6,7 @@ public class ImageUtils {
 	public final static String LOGIN_LOGO = IMAGE_PATH+"logo.png";
 	public final static String HEADER_LOGO = IMAGE_PATH+"logo-header.png";
 	public final static String C2P_HEADER_LOGO = IMAGE_PATH+"c2p_logo.jpg";
+	public final static String C22_HEADER_LOGO = IMAGE_PATH+"c2c_new_logo.png";
 	public final static String PROFILE_LOGO = IMAGE_PATH+"profile.png";
 	public final static String TAB_MONITOR = IMAGE_PATH+"tabMonitor.png";
 	public final static String TAB_CONFIG = IMAGE_PATH+"tabConfig.png";
