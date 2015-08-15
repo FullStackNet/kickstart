7c99166febf853a82ddaefdd5b8c204307e9fed4
c4t-ajay <ajay@cloud4things.com>
2015-08-15 23:40:05 +0530

    added some fixes to result screen

--- src/platform/communication/RestClient.java
+++ src/platform/communication/RestClient.java
@@ -11,14 +11,14 @@ public class RestClient {
 	String namespace;
 
 	private static RestClient instance; 
-	
+
 	public static RestClient getInstance() {
 		if (instance == null) {
 			instance = new RestClient();
 		}
 		return instance;
 	}
-	
+
 	public String getSession_id() {
 		return session_id;
 	}
@@ -47,27 +47,25 @@ public class RestClient {
 		server_url = "http://api.connect2community.in/";
 		this.namespace = "c4t"; 
 	}
-	
+
 	public RestClient(String server_url) {
 		this();
 		this.server_url = server_url;
 	}
-	
+
 	public RestClient(String server_url,String namespace) {
 		this(server_url);
 		this.namespace = namespace;
 	}
 
-	public Baseresult post(String resourceName,BaseResource resource,String action) {
-		return post(resourceName,resource,action,namespace) ;
-	}
+
 	public Baseresult get(String resourceName,String queryId, String args,Class<?> resultType) {
 		return get(resourceName, queryId, args,resultType,namespace);
 	}
 	public Baseresult getById(String resourceName, String id, Class<?> resultType) {
 		return getById(resourceName, id, resultType,namespace);
 	}	
-	
+
 	public Baseresult getById(String resourceName, String id, Class<?> resultType,String _namespace) {
 		Baseresult _result;
 		try {
@@ -83,7 +81,7 @@ public class RestClient {
 			return _result;
 		}
 	}
-	
+
 	public Baseresult get(String resourceName,String queryId, String args, Class<?> resultType,String _namespace) {
 		Baseresult _result;
 		try {
@@ -99,22 +97,20 @@ public class RestClient {
 			return _result;
 		}
 	}
-	
-	
-	public Baseresult post(String resourceName,BaseResource resource,String action,String _namespace) {
+
+	public Baseresult post(String resourceName,BaseResource resource,Class<?> resultClass,String action) {
+		return post(resourceName,resource,action,resultClass,namespace) ;
+	}
+	public Baseresult post(String resourceName,BaseResource resource,String action,Class<?> resultClass,String _namespace) {
 		String resourceString = Json.resourcetoString(resource);
 		Baseresult _result;
 		try {
+			System.out.println("request : "+resourceString + ",action : " + action);
 			String resultString = HttpClient.sendPostRequest(server_url, _namespace, session_id,
 					resourceName, resourceString, action);
-			System.out.println(resultString);
-			if (resource.getResultClass() != null) {
-				_result =  Json.stringToBaseResult(resultString, resource.getResultClass());
-				return _result;
-			} else {
-				_result =  Json.stringToBaseResult(resultString, result.class);
-				return _result;
-			}
+			System.out.println("Response : "+resultString);
+			_result =  Json.stringToBaseResult(resultString, resultClass);
+			return _result;
 		} catch(Exception e) {
 			e.printStackTrace();
 			_result = new result();
