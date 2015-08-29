e7f5a798de47fdc157ea12ec86a5a95e9ef47f8a
c4t-ajay <ajay@cloud4things.com>
2015-08-29 17:00:47 +0530

    added url debug

--- src/platform/email/SMSDispatcher.java
+++ src/platform/email/SMSDispatcher.java
@@ -117,7 +117,7 @@ public class SMSDispatcher {
 		if (!Util.isEmpty(_account.getSignature_field())) {
 			url = url + "&"+_account.getSignature_field()+"="+_account.getSignature();
 		}
-		System.out.println(url);
+		System.out.println(_account.getName() + "->"+url);
 		return url;
 	}
 
