9a056e6681df83ecd79f1d430d434dd29b7f9261
c4t-ajay <ajay@cloud4things.com>
2015-09-26 12:58:57 +0530

    added getPhotos

--- src/application/c4t/vehicle/school/helper/Student_mapHelper.java
+++ src/application/c4t/vehicle/school/helper/Student_mapHelper.java
@@ -10,22 +10,23 @@ import platform.resource.BaseResource;
 import platform.util.ApplicationException;
 import application.c4t.vehicle.school.resource.notice;
 import application.c4t.vehicle.school.resource.student_map;
+import application.c4t.vehicle.school.resource.student_photo;
 
 
 public class Student_mapHelper extends BaseHelper {
-	
+
 	Student_mapHelper() {
 		super(new student_map());
 		// TODO Auto-generated constructor stub
 	}
 	private static Student_mapHelper instance;
-	
+
 	public static Student_mapHelper getInstance() {
 		if (instance == null)
 			instance = new Student_mapHelper();
 		return instance;
 	}
-	
+
 	public void addUser(String studentId,String userId) throws ApplicationException {
 		if (studentId == null) return;
 		if (userId == null) return;
@@ -34,7 +35,25 @@ public class Student_mapHelper extends BaseHelper {
 		_map.addUsers(userId);
 		AddOrUpdate(_map);
 	}
-	
+
+	public void addPhoto(String studentId,String photoId) throws ApplicationException {
+		if (studentId == null) return;
+		if (photoId == null) return;
+		student_map _map = new student_map();
+		_map.setId(studentId);
+		_map.addPhotos(photoId);
+		AddOrUpdate(_map);
+	}
+
+	public void removePhoto(String studentId,String photoId) throws ApplicationException {
+		if (studentId == null) return;
+		if (photoId == null) return;
+		student_map _map = new student_map();
+		_map.setId(studentId);
+		_map.addPhotos(photoId);
+		unset(_map);
+	}
+
 	public void removeUser(String studentId,String userId) throws ApplicationException {
 		if (studentId == null) return;
 		if (userId == null) return;
@@ -43,7 +62,7 @@ public class Student_mapHelper extends BaseHelper {
 		_map.addUsers(userId);
 		unset(_map);
 	}
-	
+
 	public void addNotice(String studentId,String noticeId) throws ApplicationException {
 		if (studentId == null) return;
 		if (noticeId == null) return;
@@ -52,7 +71,7 @@ public class Student_mapHelper extends BaseHelper {
 		_map.addNotices(noticeId);
 		AddOrUpdate(_map);
 	}
-	
+
 	public void addParent(String studentId,String parentId) throws ApplicationException {
 		if (studentId == null) return;
 		if (parentId == null) return;
@@ -61,7 +80,7 @@ public class Student_mapHelper extends BaseHelper {
 		_map.addParents(parentId);
 		AddOrUpdate(_map);
 	}
-	
+
 	public ArrayList<BaseResource> getUsersList(String studentId) {
 		ArrayList<BaseResource>  map = new ArrayList<BaseResource>();
 		student_map _map = (student_map)getById(studentId);
@@ -69,19 +88,34 @@ public class Student_mapHelper extends BaseHelper {
 			return map;
 		return UserHelper.getInstance().getListById(_map.getUsers().toArray(new String[_map.getUsers().size()]));
 	}
-	
+
 	public BaseResource[] getNotices(String studentId) {
 		student_map _map = (student_map)getById(studentId);
 		if ((_map == null) || (_map.getNotices() == null))
 			return null;
 		return NoticeHelper.getInstance().getById(_map.getNotices().toArray(new String[_map.getNotices().size()]),new String[]{notice.FIELD_NOTICE_DATE + " desc"});
 	}
-	
+
 	public Map<String,BaseResource> getUsersMap(String studentId) {
 		Map<String,BaseResource>  map = new HashMap<String,BaseResource>();
 		student_map _map = (student_map)getById(studentId);
 		if ((_map == null) || (_map.getUsers() == null))
 			return map;
 		return UserHelper.getInstance().getMapById(_map.getUsers().toArray(new String[_map.getUsers().size()]));
+	}
+
+	public BaseResource[] getPhoto(String studentId) {
+		student_map _map = (student_map)getById(studentId);
+		if ((_map == null) || (_map.getPhotos() == null))
+			return null;
+		BaseResource[] resources = new student_photo[_map.getPhotos().size()];
+		int i = 0;
+		for(Object photo: _map.getPhotos()) { 
+			student_photo _photo = new student_photo();
+			_photo.setPhoto_id(photo.toString());
+			resources[i] = _photo;
+			i++;
+		}
+		return resources;	
 	}
 }
