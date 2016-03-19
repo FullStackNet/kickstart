919481cc924b4770bdb9e4c01ad2881a86a8fec1
c4t-ajay <ajay@cloud4things.com>
2016-03-19 23:34:48 +0530

    sync requirement

--- src/platform/webservice/service/SyncService.java
+++ src/platform/webservice/service/SyncService.java
@@ -4,10 +4,14 @@ import java.util.Map;
 
 import platform.exception.ExceptionEnum;
 import platform.helper.C4t_objectHelper;
+import platform.helper.C4t_relationHelper;
 import platform.helper.CustomerHelper;
+import platform.helper.User_mapHelper;
 import platform.resource.BaseResource;
 import platform.resource.c4t_object;
+import platform.resource.c4t_relation;
 import platform.resource.customer;
+import platform.resource.user_map;
 import platform.util.ApplicationException;
 import platform.util.ExceptionSeverity;
 import platform.util.Json;
@@ -25,7 +29,17 @@ public class SyncService extends BaseService{
 		if (_object.getType().equalsIgnoreCase("c4t_object")) {
 			c4t_object _obj = (c4t_object)Json.stringToResource(_object.getDescription(), c4t_object.class);
 			C4t_objectHelper.getInstance().AddOrUpdate(_obj);
+		} else if (_object.getType().equalsIgnoreCase("customer")) {
+			customer _obj = (customer)Json.stringToResource(_object.getDescription(), customer.class);
+			CustomerHelper.getInstance().AddOrUpdate(_obj);
+		}  else if (_object.getType().equalsIgnoreCase("user_map")) {
+			user_map _obj = (user_map)Json.stringToResource(_object.getDescription(), user_map.class);
+			User_mapHelper.getInstance().AddOrUpdate(_obj);
+		}  else if (_object.getType().equalsIgnoreCase("c4t_relation")) {
+			c4t_relation _obj = (c4t_relation)Json.stringToResource(_object.getDescription(), c4t_relation.class);
+			C4t_relationHelper.getInstance().AddOrUpdate(_obj);
 		}
+		
 	}
 
 	public BaseResource[] getQuery(ServletContext ctx, String queryId, Map<String, Object> map) throws ApplicationException {
