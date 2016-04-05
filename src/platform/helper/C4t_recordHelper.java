5b592e5184155353631748b6d4cc71288e555f0a
c4t-ajay <ajay@cloud4things.com>
2016-04-06 04:47:58 +0530

    adde new api

--- src/platform/helper/C4t_recordHelper.java
+++ src/platform/helper/C4t_recordHelper.java
@@ -128,6 +128,22 @@ public class C4t_recordHelper extends BaseHelper {
 			
 	}
 	
+	public BaseResource[] getRecordByCommunityIdForDate(String communityId,String record_type,
+			String[] order,long from_time, long to_time, Expression expression) {
+			Expression e1 = new Expression(c4t_record.FIELD_RECORD_TYPE, REL_OP.EQ, record_type);
+			Expression e2 = new Expression(c4t_record.FIELD_COMMUNITY_ID, REL_OP.EQ, communityId);
+			Expression e3 = new Expression(e1, LOG_OP.AND, e2);
+			Expression e4 = new Expression(c4t_record.FIELD_EVENT_DATE, REL_OP.GTEQ, from_time);
+			Expression e5 = new Expression(c4t_record.FIELD_EVENT_DATE, REL_OP.LT, to_time);
+			Expression e6 = new Expression(e4, LOG_OP.AND, e5);
+			Expression e7 = new Expression(e3, LOG_OP.AND, e6);
+			Expression e = e7;
+			if (expression != null) {
+				e = new Expression(e7, LOG_OP.AND, expression);
+			}
+			return getByExpression(e,order);
+	}
+	
 	public ArrayList<Map<String, Object>> getByListMapCommunityIdForDate(String communityId,String record_type,
 			String[] order,long from_time, long to_time, Expression expression) {
 			ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
