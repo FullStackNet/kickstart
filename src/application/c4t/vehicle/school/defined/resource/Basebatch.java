62019b3cc40224c1732beb99fb93595a1c66eb45
c4t-ajay <ajay@cloud4things.com>
2016-05-28 22:41:26 +0530

    added batch

---
+++ src/application/c4t/vehicle/school/defined/resource/Basebatch.java
@@ -0,0 +1,361 @@
+/*
+ * Copyright 2010-2020 M16, Inc. All rights reserved.
+ * This software and documentation contain valuable trade
+ * secrets and proprietary property belonging to M16, Inc.
+ * None of this software and documentation may be copied,
+ * duplicated or disclosed without the express
+ * written permission of M16, Inc.
+ */
+
+package application.c4t.vehicle.school.defined.resource;
+
+import platform.resource.BaseResource;
+import platform.util.*;
+import platform.db.*;
+import java.util.*;
+
+/*
+ ********** This is a generated class **********
+ * 
+ */
+public abstract class Basebatch extends BaseResource {
+	private String id = null;
+	private String name = null;
+	private String school_id = null;
+	private String customer_id = null;
+	private Long start_date = null;
+	private String start_date_str = null;
+	private Long end_date = null;
+	private String end_date_str = null;
+	private Map<String, Object> extra_data = null;
+
+	public static String FIELD_ID = "id";
+	public static String FIELD_NAME = "name";
+	public static String FIELD_SCHOOL_ID = "school_id";
+	public static String FIELD_CUSTOMER_ID = "customer_id";
+	public static String FIELD_START_DATE = "start_date";
+	public static String FIELD_START_DATE_STR = "start_date_str";
+	public static String FIELD_END_DATE = "end_date";
+	public static String FIELD_END_DATE_STR = "end_date_str";
+	public static String FIELD_EXTRA_DATA = "extra_data";
+
+	private static final long serialVersionUID = 1L;
+	private final static ResourceMetaData metaData = new ResourceMetaData("batch");
+
+	static {
+		metaData.setCheckBeforeAdd(false);
+		metaData.setCheckBeforeUpdate(false);
+		Field idField = new Field("id", "String");
+		idField.setRequired(true);
+		idField.setLength(128);
+		metaData.addField(idField);
+
+		Field nameField = new Field("name", "String");
+		nameField.setLength(128);
+		metaData.addField(nameField);
+
+		Field school_idField = new Field("school_id", "String");
+		school_idField.setLength(128);
+		metaData.addField(school_idField);
+
+		Field customer_idField = new Field("customer_id", "String");
+		customer_idField.setLength(128);
+		metaData.addField(customer_idField);
+
+		Field start_dateField = new Field("start_date", "timestamp");
+		metaData.addField(start_dateField);
+
+		Field start_date_strField = new Field("start_date_str", "String");
+		start_date_strField.setLength(32);
+		metaData.addField(start_date_strField);
+
+		Field end_dateField = new Field("end_date", "timestamp");
+		metaData.addField(end_dateField);
+
+		Field end_date_strField = new Field("end_date_str", "String");
+		end_date_strField.setLength(32);
+		metaData.addField(end_date_strField);
+
+		Field extra_dataField = new Field("extra_data", "Map");
+		extra_dataField.setValueType("Object");
+		metaData.addField(extra_dataField);
+
+
+		metaData.setTableName("batch");
+
+		metaData.setCluster("DB_PROFILE");
+	}
+
+	public Basebatch() {}
+
+	public Basebatch(Basebatch obj) {
+		this.id = obj.id;
+		this.name = obj.name;
+		this.school_id = obj.school_id;
+		this.customer_id = obj.customer_id;
+		this.start_date = obj.start_date;
+		this.start_date_str = obj.start_date_str;
+		this.end_date = obj.end_date;
+		this.end_date_str = obj.end_date_str;
+		this.extra_data = obj.extra_data;
+	}
+
+	public ResourceMetaData getMetaData() {
+		return metaData;
+	}
+
+	public Map<String, Object> convertResourceToMap() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if(id != null)
+			map.put("id", id);
+		if(name != null)
+			map.put("name", name);
+		if(school_id != null)
+			map.put("school_id", school_id);
+		if(customer_id != null)
+			map.put("customer_id", customer_id);
+		if(start_date != null)
+			map.put("start_date", start_date);
+		if(start_date_str != null)
+			map.put("start_date_str", start_date_str);
+		if(end_date != null)
+			map.put("end_date", end_date);
+		if(end_date_str != null)
+			map.put("end_date_str", end_date_str);
+		if(extra_data != null)
+			map.put("extra_data", extra_data);
+		return map;
+	}
+
+	public Map<String, Object> validateAndConvertResourceToMap(boolean add) throws ApplicationException {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if(validateId(add))
+			map.put("id", id);
+		if(name != null)
+			map.put("name", name);
+		if(school_id != null)
+			map.put("school_id", school_id);
+		if(customer_id != null)
+			map.put("customer_id", customer_id);
+		if(start_date != null)
+			map.put("start_date", start_date);
+		if(start_date_str != null)
+			map.put("start_date_str", start_date_str);
+		if(end_date != null)
+			map.put("end_date", end_date);
+		if(end_date_str != null)
+			map.put("end_date_str", end_date_str);
+		if(extra_data != null)
+			map.put("extra_data", extra_data);
+		return map;
+	}
+
+	public Map<String, Object> convertResourceToPrimaryMap() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		return map;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void convertMapToResource(Map<String, Object> map) {
+		id = (String) map.get("id");
+		name = (String) map.get("name");
+		school_id = (String) map.get("school_id");
+		customer_id = (String) map.get("customer_id");
+		start_date = (Long) map.get("start_date");
+		start_date_str = (String) map.get("start_date_str");
+		end_date = (Long) map.get("end_date");
+		end_date_str = (String) map.get("end_date_str");
+		extra_data = (Map<String, Object>) map.get("extra_data");
+	}
+
+	@SuppressWarnings("unchecked")
+	public void convertTypeUnsafeMapToResource(Map<String, Object> map) {
+		Object idObj = map.get("id");
+		if(idObj != null)
+			id = idObj.toString();
+
+		Object nameObj = map.get("name");
+		if(nameObj != null)
+			name = nameObj.toString();
+
+		Object school_idObj = map.get("school_id");
+		if(school_idObj != null)
+			school_id = school_idObj.toString();
+
+		Object customer_idObj = map.get("customer_id");
+		if(customer_idObj != null)
+			customer_id = customer_idObj.toString();
+
+		Object start_dateObj = map.get("start_date");
+		if(start_dateObj != null)
+			start_date = (Long) start_dateObj;
+
+		Object start_date_strObj = map.get("start_date_str");
+		if(start_date_strObj != null)
+			start_date_str = start_date_strObj.toString();
+
+		Object end_dateObj = map.get("end_date");
+		if(end_dateObj != null)
+			end_date = (Long) end_dateObj;
+
+		Object end_date_strObj = map.get("end_date_str");
+		if(end_date_strObj != null)
+			end_date_str = end_date_strObj.toString();
+
+		extra_data = (Map<String, Object>) map.get("extra_data");
+	}
+
+	public void convertPrimaryMapToResource(Map<String, Object> map) {
+	}
+
+	public void convertTypeUnsafePrimaryMapToResource(Map<String, Object> map) {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getIdEx() {
+		return id != null ? id : "";
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void unSetId() {
+		this.id = null;
+	}
+
+	public boolean validateId(boolean add) throws ApplicationException {
+		if(add && id == null)
+			throw new ApplicationException(ExceptionSeverity.ERROR, "Requierd validation Failed[id]");
+		return id != null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getNameEx() {
+		return name != null ? name : "";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void unSetName() {
+		this.name = null;
+	}
+
+	public String getSchool_id() {
+		return school_id;
+	}
+
+	public String getSchool_idEx() {
+		return school_id != null ? school_id : "";
+	}
+
+	public void setSchool_id(String school_id) {
+		this.school_id = school_id;
+	}
+
+	public void unSetSchool_id() {
+		this.school_id = null;
+	}
+
+	public String getCustomer_id() {
+		return customer_id;
+	}
+
+	public String getCustomer_idEx() {
+		return customer_id != null ? customer_id : "";
+	}
+
+	public void setCustomer_id(String customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	public void unSetCustomer_id() {
+		this.customer_id = null;
+	}
+
+	public Long getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Long start_date) {
+		this.start_date = start_date;
+	}
+
+
+	public String getStart_date_str() {
+		return start_date_str;
+	}
+
+	public String getStart_date_strEx() {
+		return start_date_str != null ? start_date_str : "";
+	}
+
+	public void setStart_date_str(String start_date_str) {
+		this.start_date_str = start_date_str;
+	}
+
+	public void unSetStart_date_str() {
+		this.start_date_str = null;
+	}
+
+	public Long getEnd_date() {
+		return end_date;
+	}
+
+	public void setEnd_date(Long end_date) {
+		this.end_date = end_date;
+	}
+
+
+	public String getEnd_date_str() {
+		return end_date_str;
+	}
+
+	public String getEnd_date_strEx() {
+		return end_date_str != null ? end_date_str : "";
+	}
+
+	public void setEnd_date_str(String end_date_str) {
+		this.end_date_str = end_date_str;
+	}
+
+	public void unSetEnd_date_str() {
+		this.end_date_str = null;
+	}
+
+	public Map<String, Object> getExtra_data() {
+		return extra_data;
+	}
+
+	public Object getExtra_data(String key) {
+		return extra_data == null ? null : extra_data.get(key);
+	}
+
+	public void setExtra_data(Map<String, Object> extra_data) {
+		this.extra_data = extra_data;
+	}
+
+	public void setExtra_data(String key, Object value) {
+		if(extra_data == null)
+			extra_data = new HashMap<String, Object>();
+		extra_data.put(key, value);
+	}
+
+	public void unSetExtra_data() {
+		this.extra_data = null;
+	}
+	public String getCluster() {
+		return "DB_PROFILE";
+	}
+	public String getClusterType() {
+		return "REPLICATED";
+	}
+}
