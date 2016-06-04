489d6977e59b2d2f33de94944cce7e6531cd4995
c4t-ajay <ajay@cloud4things.com>
2016-06-04 10:50:20 +0530

    added new options

--- src/application/c4t/vehicle/school/helper/Objective_question_answerHelper.java
+++ src/application/c4t/vehicle/school/helper/Objective_question_answerHelper.java
@@ -1,7 +1,12 @@
 package application.c4t.vehicle.school.helper;
 
+import platform.db.Expression;
+import platform.db.REL_OP;
 import platform.helper.BaseHelper;
+import platform.resource.BaseResource;
 import application.c4t.vehicle.school.resource.objective_question_answer;
+import application.c4t.vehicle.school.resource.test_allocation;
+import application.c4t.vehicle.school.resource.test_allocation_student;
 
 
 public class Objective_question_answerHelper extends BaseHelper {
@@ -17,4 +22,10 @@ public class Objective_question_answerHelper extends BaseHelper {
 			instance = new Objective_question_answerHelper();
 		return instance;
 	}
+	
+	public BaseResource[] getAnswersByAllocationId(String allocationId) {
+		Expression e = new Expression(objective_question_answer.FIELD_ALLOCATED_TEST_ID, REL_OP.EQ, allocationId);
+		return getByExpression(e);
+	}
+	
 }
