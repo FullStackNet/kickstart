105387b368811aade98afe2429c6cb393b1c0467
ajay.nema <ajay.nema@overturenetworks.com>
2016-06-27 08:34:54 +0530

    added new utils class

--- src/platform/util/Util.java
+++ src/platform/util/Util.java
@@ -1,7 +1,5 @@
 package platform.util;
 
-import java.awt.Color;
-import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
@@ -35,18 +33,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 
-import javax.imageio.ImageIO;
-
-import net.coobird.thumbnailator.Thumbnails;
-import net.coobird.thumbnailator.filters.Canvas;
-import net.coobird.thumbnailator.geometry.Positions;
-
-import org.imgscalr.Scalr;
+import com.eaio.uuid.UUID;
 
 import platform.log.ApplicationLogger;
 import platform.resource.c4t_object;
-
-import com.eaio.uuid.UUID;
 
 public class Util {
 	public static Long uniqueNumber = new Long(0); 
@@ -1427,29 +1417,7 @@ public class Util {
 		return list.toArray(new String[list.size()]);
 	}
 
-	private static BufferedImage resizeImage(BufferedImage image, int width, int height) throws IOException {
-		return Thumbnails.of(image)
-				.size(width, height)
-				.addFilter(new Canvas(width, height, Positions.CENTER, Color.white))
-				.asBufferedImage();
-	}
-
-	public static ByteArrayOutputStream minifyImageWidth(BufferedImage image, Integer width, Integer height) throws IOException {
-		ByteArrayOutputStream outputStreams = null;
-		outputStreams = new ByteArrayOutputStream();
-		int originalWidth = image.getWidth();
-		boolean fitExact = height == null ? false : true;
-		BufferedImage newImage;
-		newImage = image;
-		outputStreams = new ByteArrayOutputStream();
-		if (fitExact && height != null && height > 0) {
-			newImage = resizeImage(image, width, height);
-		} else if (originalWidth > width) {
-			newImage = Scalr.resize(image, Scalr.Mode.FIT_TO_WIDTH, width);
-		}
-		ImageIO.write(newImage, Field.MINIFIED_IMAGE_FILE_TYPE, outputStreams);
-		return outputStreams;
-	}
+	
 	public static synchronized void saveDataInFile(String filename, String data) {
 		FileWriter fileWritter;
 		try {
