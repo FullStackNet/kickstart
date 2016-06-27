845ea4757b71e1a06fb7732c0ae5b2c4b63f910c
ajay.nema <ajay.nema@overturenetworks.com>
2016-06-27 18:47:20 +0530

    added

--- src/platform/test/Testregex.java
+++ src/platform/test/Testregex.java
@@ -1,5 +1,12 @@
 package platform.test;
 
+import com.hazelcast.core.Cluster;
+
+import platform.db.Expression;
+import platform.db.REL_OP;
+import platform.helper.ClusterHelper;
+import platform.resource.BaseResource;
+import platform.resource.cluster;
 
 public class Testregex {
 	public static void main(String[] args) throws Exception {
@@ -8,6 +15,12 @@ public class Testregex {
 		String text2 = "sajay";
 		if (text1.matches("(?i:.*"+searchString+".*)")) {
 			System.out.println("Matched");
+		}
+		Expression e = new platform.db.Expression(cluster.FIELD_NAME, REL_OP.STARTWITH, "db_a");
+		BaseResource[] resources = ClusterHelper.getInstance().getPage(e, new String[]{"name"}, 0, 10);
+		for(int i=0 ; i < resources.length; i++) {
+			cluster _cluster = (cluster)resources[i];
+			System.out.println("name "+ _cluster.getName());
 		}
 	}
 }	
