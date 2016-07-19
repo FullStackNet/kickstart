7f27223fd7b4f85c0984ef02aa37e2287638ca41
ajay.nema <ajay.nema@overturenetworks.com>
2016-07-19 20:18:16 +0530

    added

--- src/application/c4t/vehicle/school/helper/BatchHelper.java
+++ src/application/c4t/vehicle/school/helper/BatchHelper.java
@@ -1,6 +1,10 @@
 package application.c4t.vehicle.school.helper;
 
+import platform.db.Expression;
+import platform.db.LOG_OP;
+import platform.db.REL_OP;
 import platform.helper.BaseHelper;
+import platform.resource.BaseResource;
 import application.c4t.vehicle.school.resource.batch;
 
 
@@ -16,5 +20,12 @@ public class BatchHelper extends BaseHelper {
 		if (instance == null)
 			instance = new BatchHelper();
 		return instance;
+	}
+	
+	public BaseResource[] getByOnlineBatchForCustomerId(String customerId) {
+		Expression e1 = new Expression("customer_id", REL_OP.EQ, customerId);
+		Expression e2 = new Expression("online", REL_OP.EQ, "Y");
+		Expression e = new Expression(e1, LOG_OP.AND, e2);
+		return getByExpression(e, new String[]{"name"});
 	}
 }
