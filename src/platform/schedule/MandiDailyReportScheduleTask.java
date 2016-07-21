139c407221048c2c7876383eda46b3a5c648b9f6
c4t-ajay <ajay@cloud4things.com>
2016-07-22 00:26:10 +0530

    added new options

--- src/platform/schedule/MandiDailyReportScheduleTask.java
+++ src/platform/schedule/MandiDailyReportScheduleTask.java
@@ -33,12 +33,12 @@ public class MandiDailyReportScheduleTask extends ScheduleTask {
 			resendMail.setTo(_community.getEmail_id());
 			resendMail.setType(ApplicationConstants.MAIL_TYPE_SEND_MANDI_DAILY_REPORT);
 			Map<String, String> map = new HashMap<String, String>();
-			long fromTime = TimeUtil.getTimeFromString(date, "GMT");
-			long toTime = TimeUtil.getTimeFromString(date, "GMT")+24*60*60*1000L;
+			long fromTime = TimeUtil.getTimeFromString(date, "IST");
+			long toTime = TimeUtil.getTimeFromString(date, "IST")+24*60*60*1000L;
 		
-			Expression e1 = new Expression(notice.FIELD_NOTICE_DATE, REL_OP.GTEQ, fromTime);
-			Expression e2 = new Expression(notice.FIELD_NOTICE_DATE, REL_OP.LT, toTime);
-			Expression e3 = new Expression(notice.FIELD_NOTICE_DATE, REL_OP.EQ, _community.getId());
+			Expression e1 = new Expression(mandi_auction.FIELD_CREATION_TIME, REL_OP.GTEQ, fromTime);
+			Expression e2 = new Expression(mandi_auction.FIELD_CREATION_TIME, REL_OP.LT, toTime);
+			Expression e3 = new Expression(mandi_auction.FIELD_COMMUNITY_ID, REL_OP.EQ, _community.getId());
 			Expression e4 = new Expression(e1, LOG_OP.AND, e2);
 			Expression e = new Expression(e3, LOG_OP.AND, e4);
 			BaseResource[] resources = Mandi_auctionHelper.getInstance().getByExpression(e,new String[]{mandi_auction.FIELD_PARTY,mandi_auction.FIELD_CREATION_TIME+" desc"}); 
