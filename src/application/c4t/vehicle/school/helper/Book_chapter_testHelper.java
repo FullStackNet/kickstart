32c5e16bea2dae01b0fa686276828f0e22b76070
ajay.nema <ajay.nema@overturenetworks.com>
2016-07-28 10:14:50 +0530

    added resource

---
+++ src/application/c4t/vehicle/school/helper/Book_chapter_testHelper.java
@@ -0,0 +1,29 @@
+package application.c4t.vehicle.school.helper;
+
+import application.c4t.vehicle.school.resource.book_chapter_test;
+import platform.db.Expression;
+import platform.db.LOG_OP;
+import platform.db.REL_OP;
+import platform.helper.BaseHelper;
+import platform.resource.BaseResource;
+
+
+public class Book_chapter_testHelper extends BaseHelper {
+	
+	Book_chapter_testHelper() {
+		super(new book_chapter_test());
+		// TODO Auto-generated constructor stub
+	}
+	private static Book_chapter_testHelper instance;
+	
+	public static Book_chapter_testHelper getInstance() {
+		if (instance == null)
+			instance = new Book_chapter_testHelper();
+		return instance;
+	}
+	
+	public BaseResource[] getTestByChapterId(String chapterId) {
+		Expression e = new Expression("chapterId", REL_OP.EQ, chapterId);
+		return getByExpression(e, new String[]{"order"});
+	}
+}
