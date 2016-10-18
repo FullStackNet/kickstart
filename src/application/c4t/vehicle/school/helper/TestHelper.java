8f9bae31c8b2cbda75166c5c401f4217b4b90913
ajay.nema <ajay.nema@overturenetworks.com>
2016-10-18 08:25:14 +0530

    added new api

--- src/application/c4t/vehicle/school/helper/TestHelper.java
+++ src/application/c4t/vehicle/school/helper/TestHelper.java
@@ -1,6 +1,7 @@
 package application.c4t.vehicle.school.helper;
 
 import platform.helper.BaseHelper;
+import platform.util.ApplicationException;
 import application.c4t.vehicle.school.resource.test;
 
 
@@ -16,5 +17,17 @@ public class TestHelper extends BaseHelper {
 		if (instance == null)
 			instance = new TestHelper();
 		return instance;
+	}
+	
+	public void incrementAttempted(String testId) {
+		incrementAttempted(testId,1);
+	}
+	public void incrementAttempted(String testId,int increment) {
+		try {
+			TestHelper.getInstance().incrementCounter(testId, test.FIELD_TOTAL_STUDENT_ATTEMPTED, increment);
+		} catch (ApplicationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 }
