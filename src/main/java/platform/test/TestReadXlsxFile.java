3a625a25c7cf721c3482bd9ea630550e8214f5e2
c4t-ajay <ajay@cloud4things.com>
2017-02-11 10:10:20 +0530

    added new files

---
+++ src/main/java/platform/test/TestReadXlsxFile.java
@@ -0,0 +1,9 @@
+package platform.test;
+
+import platform.db.XLSXReader;
+
+public class TestReadXlsxFile {
+    public static void main(String[] args) throws Exception {
+        XLSXReader.readFile("/home/ajay/test_xlsx.xlsx");
+    }
+}	
