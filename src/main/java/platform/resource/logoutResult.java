75f08aa9167316fe2604e0c20496a20c23d254a8
c4t-ajay <ajay@cloud4things.com>
2017-03-14 21:09:34 +0530

    added new files

---
+++ src/main/java/platform/resource/logoutResult.java
@@ -0,0 +1,41 @@
+package platform.resource;
+
+import platform.defined.resource.Baseresult;
+import platform.util.Util;
+
+
+public class logoutResult extends Baseresult {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     *
+     */
+    public logoutResult() {
+
+    }
+
+    public login[] resource;
+
+    public login[] getResource() {
+        return resource;
+    }
+
+    public void setResources(login[] resources) {
+        this.resource = resources;
+    }
+
+    public login getFirstResource() {
+        if (Util.isEmpty(resource))
+            return null;
+        return resource[0];
+    }
+
+    @Override
+    public Class<?> getResultClass() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
