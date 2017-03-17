e97dd0a74ea1e0e77b1173db737c4ccee33c8385
ajay.nema <ajay.nema@overturenetworks.com>
2017-03-17 21:50:16 +0530

    bring the email manager in platform

---
+++ src/main/java/platform/db/DBFReader.java
@@ -0,0 +1,50 @@
+package platform.db;
+
+import com.linuxense.javadbf.DBFException;
+import com.linuxense.javadbf.DBFField;
+import com.linuxense.javadbf.DBFUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ajay on 11/2/17.
+ */
+public class DBFReader {
+    public static ArrayList<Map<String, String>> readFile(String filename) throws Exception {
+        com.linuxense.javadbf.DBFReader reader = null;
+        ArrayList<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        try {
+            reader = new com.linuxense.javadbf.DBFReader(new FileInputStream(filename));
+            int numberOfFields = reader.getFieldCount();
+            String header[] = new String[numberOfFields];
+            for (int i = 0; i < numberOfFields; i++) {
+                DBFField field = reader.getField(i);
+                header[i] = field.getName();
+            }
+            Object[] rowObjects;
+            while ((rowObjects = reader.nextRecord()) != null) {
+                Map<String, String> map = new HashMap<String, String>();
+                for (int i = 0; i < rowObjects.length; i++) {
+                    map.put(header[i], rowObjects[i].toString());
+                }
+                list.add(map);
+            }
+        } catch (DBFException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            DBFUtils.close(reader);
+        }
+        return list;
+    }
+
+    Map<String, String> readReadHeaderFile(String filename) {
+        Map<String, String> map = new HashMap<String, String>();
+        return map;
+    }
+}
