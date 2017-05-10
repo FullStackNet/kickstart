02c520913fb016b007ffd77201821122fbf57a88
c4t-ajay <ajay@cloud4things.com>
2017-05-10 18:50:17 +0530

    added apartment

--- src/main/java/application/account/helper/LedgerHelper.java
+++ src/main/java/application/account/helper/LedgerHelper.java
@@ -22,17 +22,22 @@ public class LedgerHelper extends BaseHelper {
         // TODO Auto-generated constructor stub
     }
     
-    public BaseResource[] getSalesVouchers(String accountId) {
+    public BaseResource[] getNonSettledSalesVouchers(String accountId) {
+    	
     	Expression e1 = new Expression(ledger.FIELD_VOUCHER_TYPE, REL_OP.EQ,"SALES");
-    	Expression e2 = new Expression(ledger.FIELD_VOUCHER_TYPE, REL_OP.EQ,"Sales");
-    	Expression e = new Expression(e1, LOG_OP.AND,e2);
-    	return getByExpression(e,new String[]{ledger.FIELD_EVENT_DATE});
+    	Expression e2 = new Expression(ledger.FIELD_SETTLED, REL_OP.EQ,"N");
+    	Expression e3 = new Expression(ledger.FIELD_ACCOUNT_ID, REL_OP.EQ,accountId);
+    	Expression e4 = new Expression(e1, LOG_OP.AND,e2);
+       Expression e = new Expression(e3, LOG_OP.AND,e4);
+       return getByExpression(e,new String[]{ledger.FIELD_EVENT_DATE});
     }
     
-    public BaseResource[] getNonSalesVouchers(String accountId) {
+    public BaseResource[] getNonSettledNonSalesVouchers(String accountId) {
     	Expression e1 = new Expression(ledger.FIELD_VOUCHER_TYPE, REL_OP.NEQ,"SALES");
-    	Expression e2 = new Expression(ledger.FIELD_VOUCHER_TYPE, REL_OP.NEQ,"Sales");
-    	Expression e = new Expression(e1, LOG_OP.AND,e2);
-    	return getByExpression(e,new String[]{ledger.FIELD_EVENT_DATE});
+    	Expression e2 = new Expression(ledger.FIELD_SETTLED, REL_OP.EQ,"N");
+    	Expression e3 = new Expression(ledger.FIELD_ACCOUNT_ID, REL_OP.EQ,accountId);
+    	Expression e4 = new Expression(e1, LOG_OP.AND,e2);
+        Expression e = new Expression(e3, LOG_OP.AND,e4);
+      	return getByExpression(e,new String[]{ledger.FIELD_EVENT_DATE});
     }
 }
