7c7b298d6c7e8559a346bda9ab350d498ea6501e
ajay <ajay.nema@gmail.com>
2017-09-30 19:22:23 +0530

    added

--- kickstart-platform/src/main/java/platform/webservice/service/Appliance_time_seriesService.java
+++ kickstart-platform/src/main/java/platform/webservice/service/Appliance_time_seriesService.java
@@ -5,25 +5,34 @@ import java.util.Map;
 import platform.exception.ExceptionEnum;
 import platform.helper.Appliance_threshold_templeteHelper;
 import platform.resource.BaseResource;
+import platform.resource.appliance_threshold_templete;
 import platform.resource.appliance_time_series;
 import platform.util.ApplicationException;
 import platform.util.ExceptionSeverity;
 import platform.webservice.BaseService;
 import platform.webservice.ServletContext;
+import platform.webservice.WebServiceContants;
 
 public class Appliance_time_seriesService extends BaseService{
 	public Appliance_time_seriesService() {
-		super(Appliance_threshold_templeteHelper.getInstance(),new appliance_time_series());
+		super(Appliance_threshold_templeteHelper.getInstance(),new appliance_threshold_templete());
 	}
 
 	public void add(ServletContext ctx, BaseResource resource) throws ApplicationException {
-		getHelper().add(resource);
+		appliance_threshold_templete _resource = (appliance_threshold_templete)resource;
+		getHelper().add(_resource);
 	}
 	
 	public void update(ServletContext ctx, BaseResource resource) throws ApplicationException {
-		getHelper().update(resource);
+		appliance_threshold_templete _resource = (appliance_threshold_templete)resource;
+		getHelper().update(_resource);
 	}
-	
+
+	public void action(ServletContext ctx, BaseResource resource,String action) throws ApplicationException {
+		if (action.equalsIgnoreCase(WebServiceContants.OPERATION_MODIFY)) {
+			update(ctx, resource);
+		}
+	}
 	private enum QueryTypes {
 		
 	};
