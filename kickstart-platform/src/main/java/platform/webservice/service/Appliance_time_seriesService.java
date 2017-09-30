b213a4e4f35c339c6e503d6e3fa6c982d9e8a221
ajay <ajay.nema@gmail.com>
2017-09-30 18:27:54 +0530

    added

--- kickstart-platform/src/main/java/platform/webservice/service/Appliance_time_seriesService.java
+++ kickstart-platform/src/main/java/platform/webservice/service/Appliance_time_seriesService.java
@@ -21,61 +21,20 @@ public class Appliance_time_seriesService extends BaseService{
 	}
 
 	public void add(ServletContext ctx, BaseResource resource) throws ApplicationException {
-		throw new ApplicationException(ExceptionSeverity.ERROR, "Not Supported");
+		getHelper().add(resource);
 	}
 	
 	public void update(ServletContext ctx, BaseResource resource) throws ApplicationException {
-		throw new ApplicationException(ExceptionSeverity.ERROR, "Not Supported");
+		getHelper().update(resource);
 	}
 	
 	private enum QueryTypes {
 		
-		//Expects emailId. Returns the profile associated with this emailId.
-		QUERY_TIME_SERIES_DATA_FOR_COUNTER
 	};
 	
-	public BaseResource get(ServletContext ctx, String uid) {
-		String userId = ctx.getUserId();
-		return User_mapHelper.getInstance().getApplianace(userId, uid);
-	}
+
 	
 	public BaseResource[] getQuery(ServletContext ctx, String queryId, Map<String, Object> map) throws ApplicationException {
-		if(QueryTypes.QUERY_TIME_SERIES_DATA_FOR_COUNTER.toString().equals(queryId)) {
-			
-			System.out.println("Received Query "+queryId);
-			
-			String userId = ctx.getUserId();
-			if (userId == null) {
-				throw new ApplicationException(ExceptionSeverity.ERROR, "Session is expired or not authenticated.");
-			}
-			// Check for access for appliance
-			String applianceId = (String)map.get("applianceId");
-			if (applianceId == null) {
-				throw new ApplicationException(ExceptionSeverity.ERROR, "Required argument Appliance Id missing.");
-			}
-			appliance _appliance = (appliance)User_mapHelper.getInstance().getApplianace(userId, applianceId);
-			if (_appliance == null) {
-				throw new ApplicationException(ExceptionSeverity.ERROR, "Permission denied");
-			}
-		
-			String counter = (String)map.get("counter");
-			String applianceTimeZone = "IST";
-			if (_appliance.getTimeZone() != null)
-				applianceTimeZone = _appliance.getTimeZone();
-			
-			String fromDatestring = (String)map.get("from_date");
-			String toDatestring =  (String) map.get("to_date");
-			
-			if (fromDatestring == null) {
-				fromDatestring = TimeUtil.getDateString(applianceTimeZone, new Date().getTime());
-			}
-			
-			if (toDatestring == null) {
-				toDatestring = TimeUtil.getDateString(applianceTimeZone, new Date().getTime());
-			}
-			return Appliance_time_seriesHelper.getInstance().getArray(applianceId, counter, fromDatestring, toDatestring);
-		}
-		
 		throw new ApplicationException(ExceptionSeverity.ERROR, ExceptionEnum.INVALID_QUERY);
 	}
 }
