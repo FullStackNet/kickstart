cbaebc5b6a25da6a3b9f3521f3418a516f08ac78
ajay <ajay.nema@gmail.com>
2017-10-19 21:44:44 +0530

    added

---
+++ kickstart-platform/src/main/java/platform/defined/resource/Baseserver.java
@@ -0,0 +1,266 @@
+/*
+ * Copyright 2010-2020 M16, Inc. All rights reserved.
+ * This software and documentation contain valuable trade
+ * secrets and proprietary property belonging to M16, Inc.
+ * None of this software and documentation may be copied,
+ * duplicated or disclosed without the express
+ * written permission of M16, Inc.
+ */
+
+package platform.defined.resource;
+
+import platform.resource.BaseResource;
+import platform.util.*;
+import platform.db.*;
+import java.util.*;
+
+/*
+ ********** This is a generated class **********
+ * 
+ */
+public abstract class Baseserver extends BaseResource {
+	private String id = null;
+	private String type = null;
+	private String name = null;
+	private String url = null;
+	private Long creation_time = null;
+	private Map<String, Object> extra_data = null;
+
+	public static String FIELD_ID = "id";
+	public static String FIELD_TYPE = "type";
+	public static String FIELD_NAME = "name";
+	public static String FIELD_URL = "url";
+	public static String FIELD_CREATION_TIME = "creation_time";
+	public static String FIELD_EXTRA_DATA = "extra_data";
+
+	private static final long serialVersionUID = 1L;
+	private final static ResourceMetaData metaData = new ResourceMetaData("server");
+
+	static {
+		metaData.setCheckBeforeAdd(false);
+		metaData.setCheckBeforeUpdate(false);
+		Field idField = new Field("id", "String");
+		idField.setLength(128);
+		metaData.addField(idField);
+
+		Field typeField = new Field("type", "String");
+		typeField.setLength(16);
+		metaData.addField(typeField);
+
+		Field nameField = new Field("name", "String");
+		nameField.setLength(128);
+		metaData.addField(nameField);
+
+		Field urlField = new Field("url", "String");
+		urlField.setLength(128);
+		metaData.addField(urlField);
+
+		Field creation_timeField = new Field("creation_time", "timestamp");
+		metaData.addField(creation_timeField);
+
+		Field extra_dataField = new Field("extra_data", "Map");
+		extra_dataField.setValueType("Object");
+		metaData.addField(extra_dataField);
+
+
+		metaData.setTableName("server");
+
+		metaData.setCluster("DB_PROFILE");
+	}
+
+	public Baseserver() {}
+
+	public Baseserver(Baseserver obj) {
+		this.id = obj.id;
+		this.type = obj.type;
+		this.name = obj.name;
+		this.url = obj.url;
+		this.creation_time = obj.creation_time;
+		this.extra_data = obj.extra_data;
+	}
+
+	public ResourceMetaData getMetaData() {
+		return metaData;
+	}
+
+	public Map<String, Object> convertResourceToMap() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if(id != null)
+			map.put("id", id);
+		if(type != null)
+			map.put("type", type);
+		if(name != null)
+			map.put("name", name);
+		if(url != null)
+			map.put("url", url);
+		if(creation_time != null)
+			map.put("creation_time", creation_time);
+		if(extra_data != null)
+			map.put("extra_data", extra_data);
+		return map;
+	}
+
+	public Map<String, Object> validateAndConvertResourceToMap(boolean add) throws ApplicationException {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if(id != null)
+			map.put("id", id);
+		if(type != null)
+			map.put("type", type);
+		if(name != null)
+			map.put("name", name);
+		if(url != null)
+			map.put("url", url);
+		if(creation_time != null)
+			map.put("creation_time", creation_time);
+		if(extra_data != null)
+			map.put("extra_data", extra_data);
+		return map;
+	}
+
+	public Map<String, Object> convertResourceToPrimaryMap() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		return map;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void convertMapToResource(Map<String, Object> map) {
+		id = (String) map.get("id");
+		type = (String) map.get("type");
+		name = (String) map.get("name");
+		url = (String) map.get("url");
+		creation_time = (Long) map.get("creation_time");
+		extra_data = (Map<String, Object>) map.get("extra_data");
+	}
+
+	@SuppressWarnings("unchecked")
+	public void convertTypeUnsafeMapToResource(Map<String, Object> map) {
+		Object idObj = map.get("id");
+		if(idObj != null)
+			id = idObj.toString();
+
+		Object typeObj = map.get("type");
+		if(typeObj != null)
+			type = typeObj.toString();
+
+		Object nameObj = map.get("name");
+		if(nameObj != null)
+			name = nameObj.toString();
+
+		Object urlObj = map.get("url");
+		if(urlObj != null)
+			url = urlObj.toString();
+
+		Object creation_timeObj = map.get("creation_time");
+		if(creation_timeObj != null)
+			creation_time = new Long(creation_timeObj.toString());
+
+		extra_data = (Map<String, Object>) map.get("extra_data");
+	}
+
+	public void convertPrimaryMapToResource(Map<String, Object> map) {
+	}
+
+	public void convertTypeUnsafePrimaryMapToResource(Map<String, Object> map) {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getIdEx() {
+		return id != null ? id : "";
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void unSetId() {
+		this.id = null;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getTypeEx() {
+		return type != null ? type : "";
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void unSetType() {
+		this.type = null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getNameEx() {
+		return name != null ? name : "";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void unSetName() {
+		this.name = null;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getUrlEx() {
+		return url != null ? url : "";
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void unSetUrl() {
+		this.url = null;
+	}
+
+	public Long getCreation_time() {
+		return creation_time;
+	}
+
+	public void setCreation_time(Long creation_time) {
+		this.creation_time = creation_time;
+	}
+
+
+	public Map<String, Object> getExtra_data() {
+		return extra_data;
+	}
+
+	public Object getExtra_data(String key) {
+		return extra_data == null ? null : extra_data.get(key);
+	}
+
+	public void setExtra_data(Map<String, Object> extra_data) {
+		this.extra_data = extra_data;
+	}
+
+	public void setExtra_data(String key, Object value) {
+		if(extra_data == null)
+			extra_data = new HashMap<String, Object>();
+		extra_data.put(key, value);
+	}
+
+	public void unSetExtra_data() {
+		this.extra_data = null;
+	}
+	public String getCluster() {
+		return "DB_PROFILE";
+	}
+	public String getClusterType() {
+		return "REPLICATED";
+	}
+}
