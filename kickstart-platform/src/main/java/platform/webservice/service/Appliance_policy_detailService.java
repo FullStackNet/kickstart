d6ed0838965c5a6b295998c48d5f269fc8a6e9a7
ajay <ajay.nema@gmail.com>
2017-11-27 09:39:13 +0530

    added

--- kickstart-platform/src/main/java/platform/webservice/service/Appliance_policy_detailService.java
+++ kickstart-platform/src/main/java/platform/webservice/service/Appliance_policy_detailService.java
@@ -1,14 +1,11 @@
 package platform.webservice.service;
 
 import platform.exception.ExceptionEnum;
-import platform.helper.Appliance_policyHelper;
-import platform.helper.Appliance_policy_detailHelper;
-import platform.helper.Billing_planHelper;
-import platform.resource.BaseResource;
-import platform.resource.appliance_policy_detail;
-import platform.resource.billing_plan;
+import platform.helper.*;
+import platform.resource.*;
 import platform.util.ApplicationException;
 import platform.util.ExceptionSeverity;
+import platform.util.Util;
 import platform.webservice.BaseService;
 import platform.webservice.ServletContext;
 import platform.webservice.WebServiceContants;
@@ -23,6 +20,14 @@ public class Appliance_policy_detailService extends BaseService{
 	public void add(ServletContext ctx, BaseResource resource) throws ApplicationException {
 		appliance_policy_detail _resource = (appliance_policy_detail)resource;
 		_resource.setCustomer_id(ctx.getCustomerId());
+		if (Util.isEmpty(_resource.getThreshold_profile_id())) {
+			throw new ApplicationException(ExceptionSeverity.ERROR, "Please specify the Profile");
+		}
+		appliance_threshold_templete _profile = (appliance_threshold_templete) Appliance_threshold_templeteHelper.getInstance().getById(_resource.getThreshold_profile_id());
+		if (_profile == null) {
+			throw new ApplicationException(ExceptionSeverity.ERROR, "Invalid  Profile");
+		}
+		_resource.setThreshold_profile_name(_profile.getName());
 		getHelper().add(resource);
 	}
 
